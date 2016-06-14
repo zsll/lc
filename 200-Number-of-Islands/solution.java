f87c8ac24525e5e08366b68137ce9e07abf61006
zsll <zsll@users.noreply.github.com>
2016-06-14 07:11:42 -0700

    [200. Number of Islands][Time Limit Exceeded]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -22,21 +22,21 @@ public class Solution {
     	q.add(r); q.add(c);
     	while(q.size() > 0) {
     		int cr = q.remove(0), cc = q.remove(0);
+			grid[cr][cc] = '0';
     		if(cr > 0 && grid[cr - 1][cc] == '1') {
     			q.add(cr - 1);q.add(cc);
-    			grid[cr - 1][cc] = '0';
     		}
     		if(cr < h - 1 && grid[cr + 1][cc] == '1') {
     			q.add(cr + 1);q.add(cc);
-    			grid[cr + 1][cc] = '0';
+    			//grid[cr + 1][cc] = '0';
     		}
     		if(cc > 0 && grid[cr][cc - 1] == '1') {
     			q.add(cr);q.add(cc - 1);
-    			grid[cr][cc - 1] = '0';
+    			//grid[cr][cc - 1] = '0';
     		}
     		if(cc < w - 1 && grid[cr][cc + 1] == '1') {
     			q.add(cr);q.add(cc + 1);
-    			grid[cr][cc + 1] = '0';
+    			//grid[cr][cc + 1] = '0';
     		}
     	}
     	
