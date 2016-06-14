a98f62d7ad1a95cd7d488edda4ff138ef2faec43
zsll <zsll@users.noreply.github.com>
2016-06-14 07:13:25 -0700

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -7,6 +7,7 @@ public class Solution {
         		for(int i = 0; i < w*h; i++) {
         			int row = i/w, col = i%w;
         			if(grid[row][col] == '1') {
+        				grid[row][col] = '0';
         				BFSTraversal(grid, row, col);
         				r++;
         			}
@@ -22,21 +23,21 @@ public class Solution {
     	q.add(r); q.add(c);
     	while(q.size() > 0) {
     		int cr = q.remove(0), cc = q.remove(0);
-			grid[cr][cc] = '0';
     		if(cr > 0 && grid[cr - 1][cc] == '1') {
     			q.add(cr - 1);q.add(cc);
+    			grid[cr - 1][cc] = '0';
     		}
     		if(cr < h - 1 && grid[cr + 1][cc] == '1') {
     			q.add(cr + 1);q.add(cc);
-    			//grid[cr + 1][cc] = '0';
+    			grid[cr + 1][cc] = '0';
     		}
     		if(cc > 0 && grid[cr][cc - 1] == '1') {
     			q.add(cr);q.add(cc - 1);
-    			//grid[cr][cc - 1] = '0';
+    			grid[cr][cc - 1] = '0';
     		}
     		if(cc < w - 1 && grid[cr][cc + 1] == '1') {
     			q.add(cr);q.add(cc + 1);
-    			//grid[cr][cc + 1] = '0';
+    			grid[cr][cc + 1] = '0';
     		}
     	}
     	
