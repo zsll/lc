ce0f55caf61961267b1ae1cde8049fc3d4d67608
zsll <zsll@users.noreply.github.com>
2016-06-17 10:08:31 -0700

    [216. Combination Sum III][Accepted]committed by LeetCode Extension

--- 216-Combination-Sum-III/solution.java
+++ 216-Combination-Sum-III/solution.java
@@ -1,22 +1,32 @@
 public class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        	List<List<Integer>> res = new ArrayList<List<Integer>>();
+       List<List<Integer>> res = new ArrayList<List<Integer>>();
     	if(k > 0 && n >= k) {
     		List<Integer> curList = new ArrayList<Integer>();
-    		combinationSum3Helper(k, n, 0, 1, res, curList);
+    		combinationSum3Helper(k, n, 1, res, curList);
     	}
     	return res;
     }
     
-    void combinationSum3Helper(int k, int remain, int i, int start,  List<List<Integer>> res, List<Integer> curList) {
-    	if(remain == 0 && i == k) {
+    /**
+     * Note note i can be ignored for it equals curList length
+     * @param k
+     * @param remain
+     * @param i
+     * @param start
+     * @param res
+     * @param curList
+     */
+    void combinationSum3Helper(int k, int remain, int start,  List<List<Integer>> res, List<Integer> curList) {
+    	if(remain == 0 && curList.size() == k) {
     		//Collections.sort(curList);
     		List<Integer> clone = new ArrayList<Integer>(curList);	//NoteNote: we need a clone!
     		res.add(clone);
     	} else {
+    	//} else if (remain >= start) {
     		for(int j = start; j <= Math.min(9, remain); j++) {
     			curList.add(j);
-    			combinationSum3Helper(k, remain - j, i + 1, j + 1, res, curList);
+    			combinationSum3Helper(k, remain - j,  j + 1, res, curList);
     			curList.remove(curList.size() - 1);
     		}
     	}
