1b82c90b726875ca2dc686b31d41506a9b90f774
zsll <zsll@users.noreply.github.com>
2016-06-20 09:30:31 -0700

    [154. Find Minimum in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
+++ 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
@@ -2,10 +2,11 @@ public class Solution {
     public int findMin(int[] nums) {
         int start = 0;
 		int end = nums.length-1;
-		if(nums[start] < nums[end]) {	//just to make sure it's rotated
-			return nums[start];
-		}
+		
 		while(start < end) {
+			if(nums[start] < nums[end]) {	//just to make sure it's rotated, else since it's sorted, return nums start
+				return nums[start];
+			}
 			if(end-start==1) {
 				return nums[end];
 			}
@@ -15,19 +16,9 @@ public class Solution {
 			}
 			else if (nums[mid]<nums[end] || nums[start] > nums [mid]){
 				 end=mid;
-			} else {
-				int i = start + 1;
-				while( i <= end && nums[i] == nums[i - 1])
-				{//NoteNote:
-				i++;
-				}
-				if(i > end) {
-					return nums[end];
-				}else if (nums[i] > nums[i - 1]){
-					return nums[i - 1]; 
-				} else {
-						return nums[i]; 
-				}
+			} else {	
+				start++;
+				end--;
 			}
 		}
 		return nums[start];
