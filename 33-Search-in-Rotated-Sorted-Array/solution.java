0afc9f7a0ad92a86d4fb078003c8267ca6c9960a
zsll <zsll@users.noreply.github.com>
2016-07-18 21:41:34 -0700

    [33. Search in Rotated Sorted Array][Time Limit Exceeded]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,31 +1,32 @@
 public class Solution {
-    public int search(int[] A, int target) {
-        if (A == null || A.length == 0)
-									return -1;
-								int l = 0;
-								int r = A.length - 1;
-								while (l <= r) {
-									int m = (l + r) / 2;
-									if (A[m] == target)
-										return m;
-									if (A[m] > A[l] || A[m] > A[r]) {
-										if (A[m] > target && A[l] <= target) {
-											r = m - 1;
-										} else {
-											l = m + 1;
-										}
-									} else if (A[m] < A[l] || A[m] < A[r]) {
-										if (A[m] < target && A[r] >= target) {
-											l = m + 1;
-										} else {
-											r = m - 1;
-										}
-									
-									} else {
-									    l++;
-									    r--;
-									}
-								}
-								return -1;
+    public int search(int[] nums, int target) {
+        int res = -1;
+        if(nums != null && nums.length > 0) {
+            int l = 0, r = nums.length - 1;
+            while(l <= r) {
+                int mid = l + (r - l >> 1);
+                if(nums[mid] == target) {
+                    res = mid;
+                    break;
+                }
+                if (nums[mid] > nums[l] || nums[mid] > nums[r]) { //first part is sorted
+                    if(nums[l] <= target && target < nums[mid]) {
+                        r = mid - 1;
+                    } else {
+                        r = mid + 1;
+                    }
+                } else if (nums[mid] < nums[l] || nums[mid] < nums[r]) {  //second part is sorted
+                    if(nums[mid] < target && target <= nums[r]) {
+                        r = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                } else {
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return res;
     }
 }
