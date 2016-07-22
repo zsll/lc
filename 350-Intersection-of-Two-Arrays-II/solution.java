fbefc75da274b41e3ada48e6ff4a379952466511
zsll <zsll@users.noreply.github.com>
2016-07-21 22:59:12 -0700

    [350. Intersection of Two Arrays II][Accepted]committed by LeetCode Extension

--- 350-Intersection-of-Two-Arrays-II/solution.java
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -3,28 +3,30 @@ public class Solution {
         int [] res = new int[0];    //NoteNote
         if(nums1 != null && nums1.length > 0 
         &&nums2 != null && nums2.length > 0) {
-            Arrays.sort(nums1);
-            Arrays.sort(nums2);
-            List<Integer> l = new ArrayList<Integer>();
-            int i = 0, j = 0;
-            while(i < nums1.length && j < nums2.length) {
-                if(nums1[i] == nums2[j]) {
-                    //if(l.size() == 0 || l.get(l.size() - 1) < nums1[i]) {//NoteNote Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
-                        l.add(nums1[i]);
-                    //}
-                    i++;
-                    j++;
-                } else if(nums1[i] > nums2[j]) {
-                    j++;
-                } else {
-                    i++;
-                }
-                
+        HashMap<Integer, Integer> h1 = new HashMap<Integer, Integer>();
+        for(int i : nums1) {
+            if(!h1.containsKey(i)) {
+                h1.put(i, 0);
             }
-            res = new int[l.size()];
-            for(int k = 0; k < l.size(); k++) {
-                res[k] = l.get(k);
+            int count = h1.get(i);
+            h1.put(i, count + 1);
+        }
+        List<Integer> l = new ArrayList<Integer>();
+        
+        for(int i : nums2) {
+            if(h1.containsKey(i)) {
+                l.add(i);
+            int count = h1.get(i);
+            h1.put(i, count - 1);
+            if(h1.get(i) == 0) {
+                h1.remove(i);
             }
+            }
+        }
+         res = new int[l.size()];
+        for(int i = 0; i < l.size(); i++) {
+            res[i] = l.get(i);
+        }
         }
         return res;
     }
