be859132687667c3fa0459c4c4572aaf2bcd99b4
zsll <zsll@users.noreply.github.com>
2016-07-25 10:23:08 -0700

    [29. Divide Two Integers][Accepted]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,12 +1,13 @@
 public class Solution {
     public int divide(int dividend, int divisor) {
-        if (divisor == 0) {
-             return dividend >= 0? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
         
         if (dividend == 0) {
             return 0;
         }
+        if (divisor == 0) {
+             return dividend > 0? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        
         
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
