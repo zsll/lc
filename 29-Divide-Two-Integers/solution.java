443f8d7e0024fa069525e9a80dd291466280e51b
zsll <zsll@users.noreply.github.com>
2016-07-25 20:26:59 -0700

    [29. Divide Two Integers][Accepted]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,32 +1,34 @@
 public class Solution {
     public int divide(int dividend, int divisor) {
-        
-        if (dividend == 0) {
+        if (dividend == 0) {	//NoteNote
             return 0;
         }
-        if (divisor == 0) {
-             return dividend > 0? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
-        
-        
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
-        }
-        
-        boolean isNegative = (dividend < 0 && divisor > 0) || 
-                             (dividend > 0 && divisor < 0);
-                             
-        long a = Math.abs((long)dividend);
-        long b = Math.abs((long)divisor);
-        int result = 0;
-        while(a >= b){
-            int shift = 0;
-            while(a >= (b << shift)){
-                shift++;
-            }
-            a -= b << (shift - 1);
-            result += 1 << (shift - 1);
-        }
-        return isNegative? -result: result;
+    	if (divisor == 0) {	//NoteNote
+            return dividend > 0? Integer.MAX_VALUE : Integer.MIN_VALUE;
+       }
+       
+       
+       if (dividend == Integer.MIN_VALUE && divisor == -1) {	//NoteNote
+           return Integer.MAX_VALUE;
+       }
+       
+       boolean isNegative = (dividend < 0 && divisor > 0) || 
+                            (dividend > 0 && divisor < 0);
+                            
+       long a = Math.abs((long)dividend);	//NoteNote, convert to long
+       long b = Math.abs((long)divisor);
+       int result = 0;
+       
+       //each loop adds a_0*2^0/b to res
+       while(a >= b){
+           int shift = 0;
+           while(a >= (b << shift)){
+               shift++;
+           }
+           //After while b << shift > a, so needs to minus 1
+           a -= b << (shift - 1);
+           result += 1 << (shift - 1);
+       }
+       return isNegative? -result: result;
     }
 }
