291ddce22ad77480b41fe7bc34d6b9ae86673bc4
zsll <zsll@users.noreply.github.com>
2016-08-24 08:08:17 -0700

    [227. Basic Calculator II][Accepted]committed by LeetCode Extension

--- 227-Basic-Calculator-II/solution.java
+++ 227-Basic-Calculator-II/solution.java
@@ -1,42 +1,38 @@
 public class Solution {
-    public int calculate(String s) {
-    int result = 0;
-	String [] o = s.split("[0-9]+");	//get operators by excluding numbers
-	String [] n = s.split("[\\+\\-\\*/]");	////get operators by excluding operators
-	
-	List<Integer> numbers = new ArrayList<Integer>();
-	
-	List<String> operaters = new ArrayList<String>();
-
-	for(int i = 0; i < n.length; i++) {
-		if(n[i].trim().length() > 0) {
-			numbers.add(Integer.parseInt(n[i].trim()));
-		}
-	}
-	for(int i = 0; i < o.length; i++) {
-		if(o[i].trim().length() > 0) {
-			operaters.add(o[i].trim());
-		}
-	}
-	
-	for(int i = 0; i < operaters.size(); i++) {
-		if(operaters.get(i).equals("*") || operaters.get(i).equals("/")) {
-			int product = operaters.get(i).equals("*") ? numbers.get(i)* numbers.get(i+1) : numbers.get(i) / numbers.get(i+1); 
-			numbers.remove(i+1);
-			numbers.remove(i);
-			operaters.remove(i);
-			numbers.add(i, product);
-			i--;
-		} 
-	}
-	result = numbers.get(0);
-	for(int i = 0; i < operaters.size(); i++) {
-		if(operaters.get(i).equals("+")) {
-			result += numbers.get(i + 1);
-		} else {
-			result -= numbers.get(i + 1);
-		}
-	}
-	return result;
+    public int calculate(String s1) {
+            int res = 0;
+    if(s1 != null && s1.length() > 0) {
+    	String newS = s1.replace(" ", "");
+        String [] num = newS.split("[\\+\\-\\*/]");
+        String [] operators = newS.split("[0-9]+");
+        LinkedList<String> s = new LinkedList<String>();
+        s.add(num[0].trim());
+        for(int i = 1; i < operators.length; i++) {
+            if(operators[i].trim().equals("+") || operators[i].trim().equals("-")) {
+                s.add(operators[i].trim());
+                s.add(num[i].trim());
+            } else if(operators[i].trim().equals("*") || operators[i].trim().equals("/")) {
+                int a = Integer.parseInt(s.removeLast());
+                int b = Integer.parseInt(num[i].trim());
+                if(operators[i].trim().equals("*")) {
+                    s.add(Integer.toString(a * b));
+                } else {
+                    s.add(Integer.toString(a / b));
+                }
+            }
+        }
+        while(s.size() > 1) {
+            int a = Integer.parseInt(s.remove());
+            String oper = s.remove();
+            int b = Integer.parseInt(s.remove());
+            if(oper.equals("+")) {
+                    s.add(0, Integer.toString(a + b));
+                } else {
+                    s.add(0, Integer.toString(a - b));
+                }
+        }
+        res = Integer.parseInt(s.get(0));
+    }
+    return res;
     }
 }
