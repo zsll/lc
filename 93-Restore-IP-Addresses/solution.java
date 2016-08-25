56a57df9c6a4f79d672e71722a2fdb05e02c35e8
zsll <zsll@users.noreply.github.com>
2016-08-24 22:29:45 -0700

    [93. Restore IP Addresses][Accepted]committed by LeetCode Extension

--- 93-Restore-IP-Addresses/solution.java
+++ 93-Restore-IP-Addresses/solution.java
@@ -1,41 +1,26 @@
 public class Solution {
-
-    
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<String>();
-        restoreIpAddressesHelper(s, 0, 0, new StringBuffer(), res);
+        if(s != null && s.length() > 3) {
+            dfs(s, 0, 0, new StringBuffer(), res);
+        }
         return res;
     }
     
-    //pos is length at s, stage is 1 - 4, existing part in sb
-    void restoreIpAddressesHelper(String s, int pos, int stage,  StringBuffer sb, List<String> res) {
-        if(pos == s.length() && stage == 4) {
-            res.add(new String(sb));
-        } else if (pos < s.length() && stage < 4) {
-            for(int end = pos + 1; end <= pos + 3; end++) {
-                if(end <= s.length()) {
-                    String subS = s.substring(pos, end);
-                    int value = Integer.parseInt(subS);
-                    /**
-                     * NoteNote
-                     * Here are some quick reference example codes:
-Convert using Integer.toString(int)
-Convert using String.valueOf(int)
-Convert using new Integer(int).toString()
-Convert using String.format()
-Convert using DecimalFormat.
-Convert using StringBuffer or StringBuilder.
-Quick Solution.
-Convert with special radix (not base 10 system)*/
-                    if(Integer.toString(value).equals(subS) && value >= 0 && value <= 255) {  //To prevent cases like 025, 00, 
-                        int len = sb.length();
-                        if(stage > 0) {
-                            sb.append(".");
-                        }
-                        sb.append(subS);
-                        restoreIpAddressesHelper(s, end, stage + 1, sb, res);
-                        sb.setLength(len);  //NoteNote, set length should be pos plus stage, needs to set to original length
+    void dfs(String s, int i, int stage, StringBuffer sb, List<String> res) {
+        if(i == s.length() && stage == 4) {
+            res.add(sb.toString());
+        } else if(i < s.length() && stage < 4) {  //notenote
+            for(int end = i + 1; end <= Math.min(i + 3, s.length()); end++) {
+                int val = Integer.parseInt(s.substring(i, end));
+                if(val >= 0 && val <= 255 && Integer.toString(val).equals(s.substring(i, end))) {//Output: ["0.1.0.010","0.1.00.10","0.1.001.0","0.10.0.10","0.10.01.0","0.100.1.0","01.0.0.10","01.0.01.0","01.00.1.0","010.0.1.0"]
+                    int len = sb.length();
+                    sb.append(s.substring(i, end));
+                    if(stage != 3) {
+                        sb.append('.');
                     }
+                    dfs(s, end, stage + 1, sb, res);
+                    sb.setLength(len);
                 }
             }
         }
