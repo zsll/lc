f657383f067300b5e1d187b28e2432cd96744bec
zsll <zsll@users.noreply.github.com>
2016-08-24 21:17:54 -0700

    [228. Summary Ranges][Accepted]committed by LeetCode Extension

--- 228-Summary-Ranges/solution.java
+++ 228-Summary-Ranges/solution.java
@@ -1,28 +1,25 @@
 public class Solution {
-    public List<String> summaryRanges(int[] nums) {
+    public List<String> summaryRanges(int[] a) {
         List<String> res = new ArrayList<String>();
-    	if(nums != null && nums.length > 0) {
-    		int preS = nums[0], preE = nums[0];
-    		for(int i = 1; i < nums.length; i++) {
-    			int cur = nums[i];
-    			if(cur == preE + 1) {
-    				preE = cur;
-    			} else {
-    				if (preE == preS) {
-    					res.add(Integer.toString(preE));
-    				} else {
-    					res.add(preS + "->" + preE);
-    				}
-    				preE = cur;
-    				preS = cur;
-    			}
-    		}
-    		if (preE == preS) {
-    					res.add(Integer.toString(preE));
-    				} else {
-    					res.add(preS + "->" + preE);
-    				}
-    	}
-    	return res;
+        if(a != null && a.length > 0) {
+            int s = 0;
+            int i = 0;
+            for(i = 1; i < a.length; i++) {
+                if(a[i] > a[i - 1] + 1) {
+                    res.add(generateS(a[s], a[i - 1]));
+                    s = i;
+                }
+            }
+            res.add(generateS(a[s], a[i - 1]));
+        }
+        return res;
+    }
+    
+    String generateS(int a, int b) {
+        if(a == b) {
+            return Integer.toString(a);
+        } else {
+            return a + "->" + b;
+        }
     }
 }
