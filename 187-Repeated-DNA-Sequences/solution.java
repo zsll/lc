cec3d0906014d107c692375908370c29efbda8bc
zsll <zsll@users.noreply.github.com>
2016-08-25 23:22:40 -0700

    [187. Repeated DNA Sequences][Accepted]committed by LeetCode Extension

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,42 +1,37 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-       List<String> res = new ArrayList<String>();
-    	if(s != null && s.length() > 10) {
-	    	Map<Character, Integer> h = new HashMap<Character, Integer>();
-	    	h.put('A', 0);
-	    	h.put('C', 1);
-	    	h.put('G', 2);
-	    	h.put('T', 3);
-	    	/**
-	    	 * can't use set:
-	    	 * Wrong Answer More Details 
-
-Input:
+        List<String> res = new ArrayList<String>();
+        
+        if(s == null || s.length() < 10) {
+            return res;
+        }
+        HashMap<Character, Integer> d = new HashMap<Character, Integer>();
+        d.put('A', 0);
+        d.put('C', 1);
+        d.put('G', 2);
+        d.put('T', 3);
+        HashSet<Integer> m = new HashSet<Integer>();
+        int k = 0;
+        for(int i = 0; i < 10; i++) {
+            k = (k<<2) + d.get(s.charAt(i));    //NoteNote, needs ()
+        }
+        m.add(k);
+        for(int i = 10; i < s.length(); i++) {
+            k = ((k<<2) & 0xFFFFF) + d.get(s.charAt(i));
+            
+            if(!m.add(k) && res.indexOf(s.substring(i - 9, i + 1)) == -1) {
+                /**
+                 * Input:
 "AAAAAAAAAAAA"
 Output:
 ["AAAAAAAAAA","AAAAAAAAAA"]
 Expected:
 ["AAAAAAAAAA"]
-	    	 */
-	    	Map<Integer, Integer> dnaSum = new HashMap<Integer, Integer>();
-	    	int sum = 0;
-	    	for(int i = 0; i < 10; i++) {
-	    		sum = ( sum<<2 | h.get(s.charAt(i)) ) & 0xFFFFF;
-	    	}
-	    	dnaSum.put(sum, 1);
-	    	for(int i = 10; i < s.length(); i++) {
-	    		sum = ( sum<<2 | h.get(s.charAt(i)) ) & 0xFFFFF;
-	    		if(dnaSum.containsKey(sum) && 1 == dnaSum.get(sum)) {
-	    			res.add(s.substring(i - 9, i + 1));
-	    		} 
-	    		if (!dnaSum.containsKey(sum)) {
-	    			dnaSum.put(sum, 1);
-	    		} else {
-	    			dnaSum.put(sum, 0);	//Should be count plus 1, but does not matter
-	    		}
-	    		
-	    	}
-    	}
-    	return res;
+*/
+                res.add(s.substring(i - 9, i + 1));
+            } 
+            
+        }
+        return res;
     }
 }
