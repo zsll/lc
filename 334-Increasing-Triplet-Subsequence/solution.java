58182dc65501a0af305ffe25c41e0fb069169991
zsll <zsll@users.noreply.github.com>
2016-08-28 18:50:39 -0700

    [334. Increasing Triplet Subsequence][Accepted]committed by LeetCode Extension

--- 334-Increasing-Triplet-Subsequence/solution.java
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -1,26 +1,43 @@
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
-             boolean res = false;
+        boolean res = false;
         if(nums != null && nums.length >= 3) {
-        	int len = nums.length;
-        	int min = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, 
-        			minRight = Integer.MAX_VALUE; //This means minRight is on right of min2, and even smaller than min
-        	//Note min2 is always on right of min
-        	//So it's not totally equivalent to 2nd minimum
-        	for(int i = 0; i < len; i++) {
-        		if(nums[i] > min2) {
-        			res = true;
-        			break;
-        		} else if (min2 > nums[i] && nums[i] > min) {
-        			min2 = nums[i];
-        		} else if (nums[i] > minRight) {
-        			min = minRight;
-        			min2 = nums[i];
-        			minRight = Integer.MAX_VALUE;
-        		} else if (nums[i] <= min) {
-        			minRight = nums[i];
-        		} 
-        	}
+            int len = nums.length;
+            Integer min = null, min2 = null, minRight = null;
+            for(int i : nums) {
+                if(min == null) {
+                    min = i;
+                } else {
+                    if(min2 == null) {
+                        if(i > min) {
+                            min2 = i;
+                        } else {
+                            min = i;
+                        }
+                    } else {
+                        if(i > min2) {
+                            res = true;
+                            break;
+                        } else {
+                            if(minRight != null) {
+                                if(i > minRight) {
+                                    min = minRight;
+                                    min2 = i;
+                                    minRight = null;
+                                } else {
+                                    minRight = i;
+                                }
+                            } else {
+                                if(i > min) {
+                                    min2 = i;
+                                } else {
+                                    minRight = i;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
         return res;
     }
