a1619fbfbb4f83e16e8c1f96e9e48a4dde99dcb6
zsll <zsll@users.noreply.github.com>
2016-08-28 21:47:00 -0700

    [268. Missing Number][Accepted]committed by LeetCode Extension

--- 268-Missing-Number/solution.java
+++ 268-Missing-Number/solution.java
@@ -1,10 +1,33 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int total = (1 + n) * n / 2;
-        for(int i = 0; i < n; i++) {
-            total -= nums[i];
+        int res = 0;
+        if(nums != null && nums.length > 0) {
+            repos(nums);
+            for(int i = 0; i < nums.length; i++) {
+                if(nums[i] != i+1) {
+                    res = i + 1;
+                    break;
+                }
+            }
         }
-        return total;
+        return res;
+    }
+    
+    //put number i to pos i - 1
+    void repos(int [] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            int val = nums[i], newPos = val - 1;
+            while(newPos >= 0 && newPos < val && newPos != i && nums[newPos] != val) {
+                swap(nums, newPos, i);
+                val = nums[i];
+                newPos = val - 1;
+            }
+        }
+    }
+    
+    void swap(int [] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
