bb880d2ebab977e2966649f3c2aebfe4baf325f5
zsll <zsll@users.noreply.github.com>
2016-08-29 22:48:44 -0700

    [292. Nim Game][Time Limit Exceeded]committed by LeetCode Extension

--- 292-Nim-Game/solution.java
+++ 292-Nim-Game/solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public boolean canWinNim(int n) {
-        return n%4 > 0;
+        boolean res = true; //n < 4
+        if(n >= 4) {
+            boolean [] dp = new boolean[4];
+            dp[0] = true;
+            dp[1] = true;
+            dp[2] = true;
+            dp[3] = true;
+            for(int i = 4; i <= n; i++) {
+                dp[i%4] = false;
+                if(dp[(i - 1)%4] == false || dp[(i - 2)%4] == false || dp[(i - 3)%4] == false) {
+                    dp[i%4] = true;
+                }
+            }
+            res = dp[n%4];
+        }
+        return res;
     }
 }
