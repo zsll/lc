dedd72def7fa06106d6a06237a36829656d3c321
zsll <zsll@users.noreply.github.com>
2016-08-31 08:58:58 -0700

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,23 +1,20 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        int r = amount == 0 ? 0 :-1;
-        if(coins.length > 0 && amount > 0) {
-        	int [] dp = new int[amount + 1];
-//NoteNote: initialization
-        	for(int i = 1; i < amount + 1; i++) {
-        		dp[i] = Integer.MAX_VALUE;	//some impossible value, meaning making i amount is not possible
-        	}
-        	for(int i = 1; i < amount + 1; i++) {
-        		for(int j = 0; j < coins.length; j++) {
-	        		if(i >= coins[j] && dp[i - coins[j]] <= i - coins[j]) {
-	        			dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
-	        		}
-        		}
-        	}
-        	if(dp[amount] <= amount) {
-        		r = dp[amount];
-        	}
+        int res = 0;
+        if(amount > 0 && coins != null && coins.length > 0) {
+            res = -1;
+            int len = coins.length;
+            int [] dp = new int[amount + 1];
+            for(int i = 1; i <= amount; i++) {
+                dp[i] = i + 1;//Integer.MAX_VALUE;	//The problem is that 1 + Integer.MAX_VALUE will become negative, thus smaller than whatever
+                for(int j = 0; j < len; j++) {
+                    if(i >= coins[j] && dp[i - coins[j]] <= i - coins[j]) {
+                        dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                    }
+                }
+            }
+            res = dp[amount] == amount + 1 ? -1 : dp[amount];
         }
-        return r;
+        return res;
     }
 }
