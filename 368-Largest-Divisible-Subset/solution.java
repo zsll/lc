07771638542cda2c81885efb4bcb0bb3fb93667b
zsll <zsll@users.noreply.github.com>
2016-08-31 20:36:21 -0700

    [368. Largest Divisible Subset][Accepted]committed by LeetCode Extension

--- 368-Largest-Divisible-Subset/solution.java
+++ 368-Largest-Divisible-Subset/solution.java
@@ -3,24 +3,25 @@ public class Solution {
         List<Integer> res = new ArrayList<Integer>();
         if(nums != null && nums.length > 0) {
             Arrays.sort(nums);
+            int max = 0;
             int len = nums.length;
-            ArrayList<ArrayList<Integer>> subSet = new ArrayList<ArrayList<Integer>>();
-            subSet.add(new ArrayList<Integer>());
-            subSet.get(0).add(nums[0]);
-            for(int i = 1; i < len; i++) {
-                subSet.add(new ArrayList<Integer>());
-                subSet.get(i).add(nums[i]);
-                for(int j = 0; j <= i - 1; j++) {
-                    if(nums[i]%nums[j] == 0 && subSet.get(j).size() + 1 > subSet.get(i).size()) {
-                        subSet.get(i).clear();
-                        subSet.get(i).addAll(subSet.get(j));
-                        subSet.get(i).add(nums[i]);
-                    } 
+            HashMap<Integer, List<Integer>> h = new HashMap<Integer, List<Integer>>();    //Key is index, value is subset it belongs to
+            for(int j = 0; j < len; j++) {
+                h.put(j, new ArrayList<Integer>());
+                h.get(j).add(nums[j]);
+                for(int i = 0; i < j; i++) {
+                    if(nums[j]%nums[i] == 0) {
+                        if(h.get(i).size() + 1 > h.get(j).size()) {
+                            h.get(j).clear();
+                            h.get(j).add(nums[j]);
+                            h.get(j).addAll(h.get(i)); 
+                        }
+                    }
                 }
-            }
-            for(int i = 0; i < len; i++) {
-                if(subSet.get(i).size() > res.size()) {
-                    res = subSet.get(i);
+                
+                if(h.get(j).size() > max) {
+                    max = h.get(j).size();
+                    res = h.get(j);
                 }
             }
         }
