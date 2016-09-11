15e64431b01f91e1fb57b1866c6f5293d12575d6
zsll <zsll@users.noreply.github.com>
2016-09-11 00:40:45 -0700

    [372. Super Pow][Accepted]committed by LeetCode Extension

--- 372-Super-Pow/solution.java
+++ 372-Super-Pow/solution.java
@@ -1,16 +1,11 @@
 public class Solution {
-    //a^3 % 1337 = ( (a^1 % 1337) *(a^1 % 1337) *(a^1 % 1337) * (a^0 %1337) ) % 1337
-    //a^11 % 1337 = ( (a^10 % 1337) * (a^1 %1337) ) % 1337
-    //a^21 % 1337 = ( (a^10 % 1337) * (a^10 % 1337) * (a^1 %1337) ) % 1337
-    //a^10 % 1337 = (a%1337 * a%1337 * a%1337 ... * a%1337)%1337
-    //a^100 % 1337 = (a^10%1337 * a^10%1337 * a^10%1337 ... * a^10%1337)%1337
     public int superPow(int a, int[] b) {
         long base = a;
         long res = 1;
         for(int i = b.length - 1; i >= 0; i--) {
             long curDigitMod = 1;
             for(int j = 0; j < b[i]; j++) {
-                curDigitMod = (base%1337 * curDigitMod)%1337;
+                curDigitMod = (base%1337 * curDigitMod%1337)%1337;
             }
             res = (res*curDigitMod)%1337;
                 if(res == 0) {
