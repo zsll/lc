09adf11e84b8165eb1dae8fc0286df7fe9ea905a
zsll <zsll@users.noreply.github.com>
2016-09-10 17:10:58 -0700

    [345. Reverse Vowels of a String][Time Limit Exceeded]committed by LeetCode Extension

--- 345-Reverse-Vowels-of-a-String/solution.java
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -1,26 +1,27 @@
 public class Solution {
     public String reverseVowels(String s) {
-        String res = s;
-        if(s != null && s.length() > 1) {
-            int i = findVowel(s, 0, true);
-            int j = findVowel(s, s.length() - 1, false);
-            StringBuffer sb = new StringBuffer(s);
-            while(i < j) {
-                char c = sb.charAt(i);
-                sb.setCharAt(i, sb.charAt(j));
-                sb.setCharAt(j, c);
-                
-                 i = findVowel(s, i + 1, true);
-                 j = findVowel(s, j - 1, false);
+        HashSet<Integer> set = findVowelPos(s);
+        char [] c = s.toCharArray();
+        int i = 0, j = s.length() - 1;
+        while(i < j) {
+            while(!set.contains(i)) {
+                i++;
             }
-            res = sb.toString();
+            while(!set.contains(j)) {
+                j--;
+            }
+            if(i < j) {
+                char temp = c[i];
+                c[i] = c[j];
+                c[j] = temp;
+            }
         }
-        return res;
+        return new String(c);
     }
     
-    int findVowel(String s, int startPos, boolean forward) {
-        int res = startPos;
-        if(s != null && startPos >= 0 && startPos < s.length()) {
+    HashSet<Integer> findVowelPos(String s) {
+        HashSet<Integer> res = new HashSet<Integer>();
+        if(s != null && s.length() > 0) {
             HashSet<Character> se = new HashSet<Character>();
             se.add('a');
             se.add('i');
@@ -32,17 +33,12 @@ public class Solution {
             se.add('O');
             se.add('E');
             se.add('U');
-            if(forward) {
-                while(startPos < s.length() && !se.contains(s.charAt(startPos)) ) {
-                    startPos++;
-                }
-            } else {
-                while(startPos >= 0 && !se.contains(s.charAt(startPos)) ) {
-                    startPos--;
+            for(int i = 0; i < s.length(); i++) {
+                if(se.contains(s.charAt(i))) {
+                    res.add(i);
                 }
             }
-            res = startPos;
         }
-        return startPos;
+        return res;
     }
 }
