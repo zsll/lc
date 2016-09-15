c36dd763482cebaa1ac7cc74e9b239f84a8609ca
zsll <zsll@users.noreply.github.com>
2016-09-14 18:45:09 -0700

    [345. Reverse Vowels of a String][Accepted]committed by LeetCode Extension

--- 345-Reverse-Vowels-of-a-String/solution.java
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -1,46 +1,36 @@
 public class Solution {
     public String reverseVowels(String s) {
-        HashSet<Integer> set = findVowelPos(s);
         char [] c = s.toCharArray();
         int i = 0, j = s.length() - 1;
         while(i < j) {
-            while(!set.contains(i) && i < j) {
+            while(!isVowel(c[i]) && i < j) {
                 i++;
             }
-            while(!set.contains(j) && i < j) {
+            while(!isVowel(c[j]) && i < j) {
                 j--;
             }
             if(i < j) {
                 char temp = c[i];
                 c[i] = c[j];
                 c[j] = temp;
-                i++;
+                i++; //NoteNote 
                 j--;
             }
         }
         return new String(c);
     }
     
-    HashSet<Integer> findVowelPos(String s) {
-        HashSet<Integer> res = new HashSet<Integer>();
-        if(s != null && s.length() > 0) {
-            HashSet<Character> se = new HashSet<Character>();
-            se.add('a');
-            se.add('i');
-            se.add('o');
-            se.add('e');
-            se.add('u');
-            se.add('A');
-            se.add('I');
-            se.add('O');
-            se.add('E');
-            se.add('U');
-            for(int i = 0; i < s.length(); i++) {
-                if(se.contains(s.charAt(i))) {
-                    res.add(i);
-                }
-            }
-        }
-        return res;
+    boolean isVowel(char c) {
+        HashSet<Character> se = new HashSet<Character>(); se.add('a');
+se.add('i');
+se.add('o');
+se.add('e');
+se.add('u');
+se.add('A');
+se.add('I');
+se.add('O');
+se.add('E');
+se.add('U');
+return se.contains(c);
     }
 }
