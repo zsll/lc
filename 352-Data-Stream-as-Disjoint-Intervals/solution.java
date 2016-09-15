b129888a8a49c2045e055567c526804ea1a0a4cc
zsll <zsll@users.noreply.github.com>
2016-09-14 18:22:17 -0700

    [352. Data Stream as Disjoint Intervals][Accepted]committed by LeetCode Extension

--- 352-Data-Stream-as-Disjoint-Intervals/solution.java
+++ 352-Data-Stream-as-Disjoint-Intervals/solution.java
@@ -9,32 +9,32 @@
  */
 public class SummaryRanges {
 
-   TreeMap<Integer, Interval> tree;
+    TreeMap<Integer, Interval> tree;//key is start of interval, key is interval
 
-    public SummaryRanges() {
-        tree = new TreeMap<>();
-    }
-
-    public void addNum(int val) {
-        if(tree.containsKey(val)) return;
-        Integer l = tree.lowerKey(val);
-        Integer h = tree.higherKey(val);
-        if(l != null && h != null && tree.get(l).end + 1 == val && h == val + 1) {
-            tree.get(l).end = tree.get(h).end;
-            tree.remove(h);
-        } else if(l != null && tree.get(l).end + 1 >= val) {
-            tree.get(l).end = Math.max(tree.get(l).end, val);
-        } else if(h != null && h == val + 1) {
-            tree.put(val, new Interval(val, tree.get(h).end));
-            tree.remove(h);
-        } else {
-            tree.put(val, new Interval(val, val));
+        public SummaryRanges() {
+            tree = new TreeMap<>();
         }
-    }
 
-    public List<Interval> getIntervals() {
-        return new ArrayList<>(tree.values());
-    }
+        public void addNum(int val) {
+            if(tree.containsKey(val)) return;
+            Integer l = tree.lowerKey(val);//NoteNote
+            Integer h = tree.higherKey(val);
+            if(l != null && h != null && tree.get(l).end + 1 == val && h == val + 1) {
+                tree.get(l).end = tree.get(h).end;
+                tree.remove(h);
+            } else if(l != null && tree.get(l).end + 1 >= val) {
+                tree.get(l).end = Math.max(tree.get(l).end, val);
+            } else if(h != null && h == val + 1) {
+                tree.put(val, new Interval(val, tree.get(h).end));
+                tree.remove(h);
+            } else {
+                tree.put(val, new Interval(val, val));
+            }
+        }
+
+        public List<Interval> getIntervals() {
+            return new ArrayList<>(tree.values());
+        }
 }
 
 /**
