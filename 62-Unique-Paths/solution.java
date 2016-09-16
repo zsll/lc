59b42972790bc78c9e429f8b4cf79e301dfdbafa
zsll <zsll@users.noreply.github.com>
2016-09-15 21:00:38 -0700

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,25 +1,18 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-     int res = 0;
-    	if( m > 0 && n > 0) {
-        
-	        int[][] dp = new int[2][n + 1];
-	        
-	        // 1st row only 1 path
-	        for (int i=1; i<=n; i++)
-	            dp[1][i] = 1;
-	        
-	        // 1st column only 1 path
-	        for (int i=2; i<=m; i++)
-	            dp[i%2][1] = 1;
-	        
-	        // for each body node, number of path = paths from top + paths from left
-	        for (int i=2; i<=m; i++){
-	            for (int j=2; j<=n; j++){
-	                dp[i%2][j] = dp[(i-1)%2][j] + dp[i%2][(j-1)];
-	            }
-	        }
-	        res = dp[m%2][n];
+        int res = 0;
+    	if(m > 0 && n > 0) {
+    	    int [][] dp = new int[m][n];
+    	    for(int i = 0; i < n; i++) {
+    	        dp[0][i] = 1;
+    	    }
+    	    for(int i = 1; i < m; i++) {
+    	        dp[i][0] = 1;
+    	        for(int j = 1; j < n; j++) {
+    	            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    	        }
+    	    }
+    	    res = dp[m - 1][n - 1];
     	}
         return res;   
     }
