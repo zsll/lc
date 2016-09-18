9d80470ce3bdc8d1e2409b11298b7181fb78f826
zsll <zsll@users.noreply.github.com>
2016-09-18 15:40:50 -0700

    [113. Path Sum II][Accepted]committed by LeetCode Extension

---
+++ 113-Path-Sum-II/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> cur = new ArrayList<Integer>();
+        pathSumDFS(root, sum, res, cur);
+        return res;
+    }
+    
+    void pathSumDFS(TreeNode root, int sum, List<List<Integer>> res, List<Integer> cur) {
+        if(root != null) {
+            cur.add(root.val);
+            if(root.val == sum && root.left == null && root.right == null) {
+                res.add(new ArrayList<Integer>(cur));
+            } else {
+                sum -= root.val;
+                pathSumDFS(root.left, sum, res, cur);
+                pathSumDFS(root.right, sum, res, cur);
+            }
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
