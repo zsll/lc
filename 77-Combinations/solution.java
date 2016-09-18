4be09fd302d8aac7f49c0fd87e38e7aeb224bbea
zsll <zsll@users.noreply.github.com>
2016-09-18 14:38:41 -0700

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,25 +1,20 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
-    	List<List<Integer>> r = new ArrayList<List<Integer>>();
-    	List<Integer> c = new ArrayList<Integer>();
-    	HashSet<Integer> v = new HashSet<Integer>();
-    	combineHelperPermutationStyle(n, k, 1, r, c, v);
-    	return r;
-    }
-    
-    void combineHelperPermutationStyle(int n, int k, int start, List<List<Integer>> r, List<Integer> c, HashSet<Integer> v) {
-    	if(c.size() == k) {
-    	    r.add(new ArrayList<Integer>(c));
-    	} else {
-    	    for(int i = start; i <= n; i++) {
-    	        if(!v.contains(i)) {
-    	            c.add(i);
-    	            v.add(i);
-    	            combineHelperPermutationStyle(n, k, i + 1, r, c, v);
-    	            c.remove(c.size() - 1);
-    	            v.remove(i);
-    	        }
-    	    }
-    	}
-    }
+		List<List<Integer>> r = new ArrayList<List<Integer>>();
+		List<Integer> c = new ArrayList<Integer>();
+		combineHelperPermutationStyle(n, k, 1, r, c);
+		return r;
+	}
+
+	void combineHelperPermutationStyle(int n, int k, int start, List<List<Integer>> r, List<Integer> c) {
+		if (c.size() == k) {
+			r.add(new ArrayList<Integer>(c));
+		} else {
+			for (int i = start; i <= n; i++) {
+					c.add(i);
+					combineHelperPermutationStyle(n, k, i + 1, r, c);
+					c.remove(c.size() - 1);
+			}
+		}
+	}
 }
