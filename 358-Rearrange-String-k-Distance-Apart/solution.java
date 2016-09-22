05d8ec1ab8baf2b62f20fbd78523393988443d9d
zsll <zsll@users.noreply.github.com>
2016-09-21 20:02:29 -0700

    [358. Rearrange String k Distance Apart][Accepted]committed by LeetCode Extension

---
+++ 358-Rearrange-String-k-Distance-Apart/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+     public String rearrangeString(String str, int k) {
+            int length = str.length();
+            int[] count = new int[26];
+            int[] valid = new int[26];
+            for(int i=0;i<length;i++){
+                count[str.charAt(i)-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int index = 0;index<length;index++){
+                int candidatePos = findValidMax(count, valid, index);
+                if( candidatePos == -1) return "";
+                count[candidatePos]--;
+                valid[candidatePos] = index+k;
+                sb.append((char)('a'+candidatePos));
+            }
+            return sb.toString();
+        }
+        
+        /**
+         * index is the position to fill
+         * @param count
+         * @param valid
+         * @param index
+         * @return
+         */
+       private int findValidMax(int[] count, int[] valid, int index){
+           int max = Integer.MIN_VALUE;
+           int candidatePos = -1;
+           for(int i=0;i<count.length;i++){//There are at most 26 chars, so complexity would be at most 26N
+               if(count[i]>0 && count[i]>max && index>=valid[i]){	
+            	   //Find char with largest frequency and valid position is larger than or equal to the position to fill
+                   max = count[i];
+                   candidatePos = i;
+               }
+           }
+           return candidatePos;
+       }
+
+}
