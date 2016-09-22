0686be9cff0826ed29fcda589babca1d5ffeeddf
zsll <zsll@users.noreply.github.com>
2016-09-22 08:49:30 -0700

    [389. Find the Difference][Accepted]committed by LeetCode Extension

--- 389-Find-the-Difference/solution.java
+++ 389-Find-the-Difference/solution.java
@@ -1,26 +1,13 @@
 public class Solution {
     public char findTheDifference(String s, String t) {
-        char res = 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int res = 0;
+        
         for(char c : s.toCharArray()) {
-            if(!map.containsKey(c)) {
-                map.put(c, 0);
-            }
-            int prev = map.get(c);
-            map.put(c, prev + 1);
+            res = res^((int)c);
         }
         for(char c : t.toCharArray()) {
-            if(!map.containsKey(c)) {
-                res = c;
-                break;
-            }
-            int prev = map.get(c);
-            if(prev == 1) {
-                map.remove(c);
-            } else {
-                map.put(c, prev - 1);
-            }
+            res = res^((int)c);
         }
-        return res;
+        return (char)res;
     }
 }
