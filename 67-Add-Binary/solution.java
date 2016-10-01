2f80b10ff56b28626bab74b94f02d9236f889695
zsll <zsll@users.noreply.github.com>
2016-10-01 10:58:22 -0700

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -7,18 +7,25 @@ public class Solution {
             int sum = (ai ^ bj) ^ c;
             c = (ai + bj + c)/2;
             sb.insert(0, sum);
+            i--;
+            j--;
         }
         while(i >= 0) {
             int ai = a.charAt(i) - '0';
             int sum = ai ^ c;
             c = (ai + c)/2;
             sb.insert(0, sum);
+            i--;
         }
         while(j >= 0) {
             int bj = b.charAt(j) - '0';
             int sum = bj ^ c;
             c = (bj + c)/2;
             sb.insert(0, sum);
+            j--;
+        }
+        if(c != 0) {
+            sb.insert(0, 1);
         }
         return sb.toString();
     }
