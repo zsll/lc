c1b93c337c55b3219db087bad84e19bfb61a27ee
zsll <zsll@users.noreply.github.com>
2016-10-01 11:17:13 -0700

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -2,34 +2,30 @@ public class Solution {
     public int numDecodings(String s) {
         int res = 0;
         if(s != null && s.length() > 0) {
-            int len = s.length();
             int [] dp = new int[3];
+            dp[0] = 1;
             char [] a = s.toCharArray();
-            dp[0] = 1;  //from right, how many ways to decode from i th char to end
-            if(canUse1(a, 0)) {
-                dp[1] = 1;
-            }
-            for(int i = 2; i <= len; i++) {
-                dp[i%3] = 0;	//NoteNoe
-                if(canUse1(a,i - 1)) {
+            for(int i = 1; i <= s.length();i++) {
+                dp[i%3] = 0;
+                if(canUse1(a, i - 1)) {
                     dp[i%3] += dp[(i - 1)%3];
-                } 
+                }
                 
-                if(canUse2(a,i - 1)) {
+                if(i > 1 && canUse2(a, i - 1)) {
                     dp[i%3] += dp[(i - 2)%3];
-                } 
+                }
             }
-            res = dp[len%3];
+            res = dp[s.length()%3];
         }
         return res;
     }
     
-    boolean canUse1(char [] c, int i) {
-        return c[i]  >= '1' && c[i] <= '9';
+    boolean canUse1(char [] a, int i) {
+        return a[i] >= '1' && a[i] <= '9';
     }
     
-    boolean canUse2(char [] c, int i) {
-        int sum = c[i] - '1' + 1 + (c[i-1] - '1' + 1) * 10;
-        return canUse1(c, i-1)&&sum >= 1 && sum <= 26;
+    boolean canUse2(char [] a, int i) {
+        int sum = (a[i - 1] - '0')*10 + a[i] - '0';
+        return canUse1(a, i - 1) && sum >= 1 && sum <= 26;
     }
 }
