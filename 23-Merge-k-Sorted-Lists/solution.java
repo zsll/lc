d6ad5c700ace6a6e3818d24975957159e6d4e32a
zsll <zsll@users.noreply.github.com>
2016-10-01 12:05:51 -0700

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -8,56 +8,45 @@
  */
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode res = null;
-        if(lists != null && lists.length > 0) {
-        int len = lists.length;
-        
-        res = helper(lists, 0, len - 1);
+        if(lists == null || lists.length == 0) {
+            return null;
         }
-        return res;
+        return mergeKListsHelper(lists, 0, lists.length - 1);
     }
     
-    
-    public ListNode helper(ListNode [] lists, int start, int end) {
-        ListNode res = lists[start];
-        if(start < end) {
-            int mid = (end - start >> 1) + start;
-            ListNode a = helper(lists, start, mid);
-            ListNode b = helper(lists, mid + 1, end);
-            res = merge(a, b);
+    public ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
+        if(start == end) {
+            return lists[start];
+        } else {
+            int mid = start + (end - start >> 1);
+            ListNode a = mergeKListsHelper(lists, start, mid);
+            ListNode b = mergeKListsHelper(lists, mid + 1, end);
+            return merge(a, b);
         }
-        return res;
     }
     
     ListNode merge(ListNode a, ListNode b) {
-        ListNode dummy = new ListNode(0), end = dummy;
+        ListNode d = new ListNode(0), cur = d;
         while(a != null && b != null) {
             if(a.val < b.val) {
-                end.next = a;
+                cur.next = a;
                 a = a.next;
             } else {
-                
-                end.next = b;
+                cur.next = b;
                 b = b.next;
             }
-            
-                end = end.next;
-                end.next = null;
+            cur = cur.next;
         }
-        
         while(a != null) {
-            end.next = a;
+            cur.next = a;
             a = a.next;
-            end = end.next;
-            end.next = null;
+            cur = cur.next;
         }
-        
         while(b != null) {
-            end.next = b;
+            cur.next = b;
             b = b.next;
-            end = end.next;
-            end.next = null;
+            cur = cur.next;
         }
-        return dummy.next;
+        return d.next;
     }
 }
