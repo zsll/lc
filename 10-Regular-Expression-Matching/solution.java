94b9bb9ec7ff1b0fa386130bb9b7db2b500b49c3
zsll <zsll@users.noreply.github.com>
2016-10-01 11:44:50 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,36 +1,33 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
         int h = s.length(), w = p.length();
-        boolean [][] dp = new boolean[h + 1][w + 1];
+        boolean [][] dp = new boolean[2][w + 1];
         for(int i = 0; i <= h; i++) {
             for(int j = 0; j <= w; j++) {
+                dp[i%2][j] = false;
                 if(j == 0 && i == 0) {
-                    dp[i][j] = true;
-                } else if (j == 0) {
-                    dp[i][j] = false;
+                    dp[i%2][j] = true;
                 } else if (i == 0) {
-                    dp[i][j] = false;
                     if(j >= 2 && dp[i][j - 2] && p.charAt(j - 1) == '*' && p.charAt(j - 2) != '*') {
-                        dp[i][j] = true;
+                        dp[i%2][j] = true;
                     }
-                } else {
-                    dp[i][j] = false;
+                } else if (j > 0 && i > 0) {    //Note Note
                     if(p.charAt(j - 1) != '*') {
                         if(s.charAt(i - 1) == p.charAt(j - 1) ||  p.charAt(j - 1) == '.') {
-                            dp[i][j] = dp[i - 1][j - 1];
+                            dp[i%2][j] = dp[(i - 1)%2][j - 1];
                         }
                     } else {
                         if(j >= 2) {    //NoteNote, else must be false
                             if(p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.' ) {
-                                dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                                dp[i%2][j] = dp[i%2][j - 2] || dp[(i - 1)%2][j];
                             } else {
-                                dp[i][j] = dp[i][j - 2];
+                                dp[i%2][j] = dp[i%2][j - 2];
                             }
                         }
                     }
                 }
             }
         }
-        return dp[h][w];
+        return dp[h%2][w];
     }
 }
