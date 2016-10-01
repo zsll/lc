edd889280c6f3a536f022bad14c3e25fc5dc281a
zsll <zsll@users.noreply.github.com>
2016-10-01 12:37:11 -0700

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,21 +1,19 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
         boolean res = false;
-        if(wordDict != null && wordDict.size() > 0) {
+        if(s != null && wordDict != null) {
             int len = s.length();
             boolean [] dp = new boolean[len + 1];
             dp[0] = true;
-            for(int i = 1; i < len + 1; i++) {
+            for(int i = 1; i <= len; i++) {
                 for(String s1 : wordDict) {
                     if(i >= s1.length() && dp[i - s1.length()] && s.substring(i - s1.length(), i).equals(s1)) {
-                            dp[i] = true;
-                        break;
+                        dp[i] = true;
                     }
                 }
             }
             res = dp[len];
         }
-        
         return res;
     }
 }
