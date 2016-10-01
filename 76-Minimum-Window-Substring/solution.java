e6c64081d31b9d41507c8a38e559376582a45bad
zsll <zsll@users.noreply.github.com>
2016-10-01 12:31:13 -0700

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

---
+++ 76-Minimum-Window-Substring/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public String minWindow(String s, String t) {
+        String res = "";
+        if(!(t == null || s == null || s.length() == 0 || t.length() == 0 || s.length() < t.length())) {
+            int minLen = s.length() + 1, minStart = 0, minEnd = 0, inSize = 0, j = 0;  //minEnd = 0 means invalid
+            int [] tCnt = new int[255];
+            int [] inCnt = new int[255];
+            for(int i = 0; i < t.length(); i++) {
+                tCnt[t.charAt(i)]++;
+            }
+            for(int i = 0; i < s.length(); i++) {
+                while(inSize < t.length() && j < s.length()) {
+                    int c = (int)s.charAt(j);
+                    
+                        inCnt[c]++;
+                        if(inCnt[c] <= tCnt[c]) {
+                            inSize++;
+                        }
+                    
+                    j++;
+                }
+                if(inSize == t.length() && minLen > j - i) {
+                    minLen = j - i;
+                    minStart = i;
+                    minEnd = j;
+                }
+                int c = (int)s.charAt(i);
+                
+                    inCnt[c]--;
+                    if(tCnt[c] > 0 && inCnt[c] < tCnt[c]) {
+                        inSize--;
+                    }
+                
+            }
+            if(minEnd > 0) {
+                res = s.substring(minStart, minEnd);
+            }
+        }
+        return res;
+    }
+}
