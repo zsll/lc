92b119741b3e27bd8a3a5230d038cbbb82e14229
zsll <zsll@users.noreply.github.com>
2016-10-01 11:42:00 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,29 +1,36 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int height = s.length(),width = p.length();
-        boolean[][] dp = new boolean[height + 1][width + 1];	//please note the +1
-        dp[0][0] = true;	//both empty
-        for(int i = 2; i <= width; i++){
-            if(i%2 == 0) {
-                if(p.charAt(i - 1) == '*') dp[0][i] = true;
-                else break;
-            }	//initialize the scenario that s is empty, the first column. The rest of the first row would be false
-        }
-        for(int i = 1; i <= height; i++){	//Please note here we can scan col firstly, the two for loops can swap
-            for(int j = 1; j <= width; j++){
-                char sChar = s.charAt(i - 1);                
-                char pChar = p.charAt(j - 1);
-                if(sChar == pChar || pChar == '.'){
-                    dp[i][j] = dp[i - 1][j - 1];
-                }else if(pChar == '*'){//if current char is *
-                    if(sChar != p.charAt(j - 2) && p.charAt(j - 2) != '.'){//if current char does not match p.charAt(j - 2)*
-                        dp[i][j] = dp[i][j - 2];	//Then don't use this ?* part eg. s:bc, p:bca*
-                    }else{	//if current char s.charAt(i-1) match p.charAt(j - 2)*
-                        dp[i][j] =  dp[i][j - 2] | dp[i - 1][j];	// either don't used it s:bc, p:bca*, or it matches previous s:baa, p:ba* 
+        int h = s.length(), w = p.length();
+        boolean [][] dp = new boolean[h + 1][w + 1];
+        for(int i = 0; i <= h; i++) {
+            for(int j = 0; j <= w; j++) {
+                if(j == 0 && i == 0) {
+                    dp[i][j] = true;
+                } else if (j == 0) {
+                    dp[i][j] = false;
+                } else if (i == 0) {
+                    dp[i][j] = false;
+                    if(j >= 2 && dp[i][j - 2] && p.charAt(j - 1) == '*' && p.charAt(j - 2) != '*') {
+                        dp[i][j] = true;
+                    }
+                } else {
+                    dp[i][j] = false;
+                    if(p.charAt(j - 1) != '*') {
+                        if(s.charAt(i - 1) == p.charAt(j - 1) ||  p.charAt(j - 1) == '.') {
+                            dp[i][j] = dp[i - 1][j - 1];
+                        }
+                    } else {
+                        if(j >= 2) {    //NoteNote, else must be false
+                            if(p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.' ) {
+                                dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                            } else {
+                                dp[i][j] = dp[i][j - 2];
+                            }
+                        }
                     }
                 }
             }
         }
-        return dp[height][width];
+        return dp[h][w];
     }
 }
