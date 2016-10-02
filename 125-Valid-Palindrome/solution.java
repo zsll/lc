2a85571439bc286f68099447a361909d08da4c73
zsll <zsll@users.noreply.github.com>
2016-10-01 17:53:05 -0700

    [125. Valid Palindrome][Accepted]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -4,10 +4,10 @@ public class Solution {
         if(s != null && s.length() > 0) {
             int i = 0, j = s.length() - 1;
             while(i < j) {
-                while(!isAlpha(s.charAt(i)) && i < j) {
+                while(!isAlpha(s.charAt(i)) && !isNumber(s.charAt(i)) && i < j) {
                     i++;
                 }
-                while(!isAlpha(s.charAt(j)) && i < j) {
+                while(!isAlpha(s.charAt(j)) && !isNumber(s.charAt(j)) && i < j) {
                     j--;
                 }
                 if(i < j && !charEqauls(
@@ -16,6 +16,9 @@ public class Solution {
                 ) {
                     res = false;
                     break;
+                } else {
+                    i++;
+                    j--;
                 }
             }
         }
@@ -26,7 +29,11 @@ public class Solution {
         return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
     }
     
+    boolean isNumber(char c) {
+        return c >= '0' && c <= '9';
+    }
+    
     boolean charEqauls(char a, char b) {
-        return Character.toLowerCase(a) == Character.toLowerCase(b);
+        return isAlpha(a) ? Character.toLowerCase(a) == Character.toLowerCase(b) : a == b;
     }
 }
