59bad2b684f300d148c114644a8684ce1a382351
zsll <zsll@users.noreply.github.com>
2016-10-01 21:31:31 -0700

    [28. Implement strStr()][Time Limit Exceeded]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -4,15 +4,14 @@ public class Solution {
         if(needle.length() == 0) {
             res = 0;
         } else if (haystack.length() >= needle.length()) {
-            for(int i = 0; i < haystack.length() && res == -1; i++) {
+            for(int i = 0; i < haystack.length(); i++) {
                 int j = 0;
-                for(; j < needle.length() && i + j < haystack.length(); j++) {
-                    if(needle.charAt(j) != haystack.charAt(i + j)) {
-                        break;
-                    }
+                for(; j < needle.length() && i + j < haystack.length() && needle.charAt(j) == haystack.charAt(i + j); j++) {
+                    
                 }
                 if(j == needle.length()) {
                     res = i;
+                    break;
                 }
             }
         }
