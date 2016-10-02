d7cdb6cf383876be62de2ae62b58afd4a56f3a5d
zsll <zsll@users.noreply.github.com>
2016-10-01 17:36:14 -0700

    [43. Multiply Strings][Accepted]committed by LeetCode Extension

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -1,29 +1,42 @@
 public class Solution {
     public String multiply(String num1, String num2) {
-        String n1 = new StringBuilder(num1).reverse().toString();	//NoteNote: StringBuffer is synchronized, StringBuilder is not.
-		String n2 = new StringBuilder(num2).reverse().toString();
-		
-		int[] d = new int[n1.length()+n2.length()];		// 构建数组存放乘积 NoteNote
-		for(int i=0; i<n1.length(); i++){
-			for(int j=0; j<n2.length(); j++){
-				d[i+j] += (n1.charAt(i)-'0') * (n2.charAt(j)-'0');		// 在正确位置累加乘积
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<d.length; i++){
-			int digit = d[i]%10;		// 当前位
-			int carry = d[i]/10;		// 进位
-			if(i+1<d.length){
-				d[i+1] += carry;
-			}
-			sb.insert(0, digit);		// prepend
-		}
-		
-		// 移除前导零
-		while(sb.charAt(0)=='0' && sb.length()>1){
-			sb.deleteCharAt(0);
-		}
-		return sb.toString();
+        StringBuffer sb = new StringBuffer();
+        if(num1 != null && num2 != null && num1.length() > 0 && num2.length() > 0) {
+            int len1 = num1.length(), len2 = num2.length();
+            int [] res = new int[len1 + len2];
+            
+            /**
+             * Submission Result: Wrong Answer More Details 
+
+Input:
+"98"
+"9"
+Output:
+"801"
+Expected:
+"882"
+             */
+            num1 = new String((new StringBuffer(num1)).reverse());
+            num2 = new String((new StringBuffer(num2)).reverse());
+            for(int i = len1 - 1; i >=0 ; i--) {
+                for(int j = len2 - 1; j >= 0; j--) {
+                    res[i + j] += (num1.charAt(i) - '0')*(num2.charAt(j) - '0');//NoteNote: should be +=
+                }
+            }
+            for(int i = 0; i < len1 + len2 - 1; i++) {
+                //if(i < len1 + len2 - 1) {
+                    res[i + 1] += res[i]/10;
+                //}
+                res[i] %= 10;
+            }
+            int start = len1 + len2 - 1;
+            while(res[start] == 0 && start > 0) {
+                start--;
+            }
+            for(int i = start; i >= 0; i--) {
+                sb.append(res[i]);
+            }
+        }
+        return sb.toString();
     }
 }
