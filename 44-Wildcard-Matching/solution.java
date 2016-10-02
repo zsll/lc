31a514d149e0c76f26157b0e9cdc8bc3a28c8932
zsll <zsll@users.noreply.github.com>
2016-10-02 00:23:39 -0700

    [44. Wildcard Matching][Time Limit Exceeded]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,44 +1,34 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
         boolean res = true;
-    	if(s == null || p == null) {
-    		res = true;
-    	} else {
-    		int sLen = s.length(), pLen = p.length();
-    		boolean [][] dp = new boolean[sLen + 1][2];
-    		dp[0][0] = true;
-    		for(int j = 1; j <= pLen; j++) {
-    			char pChar = p.charAt(j - 1);
-    			if(pChar != '*') {
-    				for(int i = 0; i <= sLen; i++) {
-    					if(i == 0) {
-    						dp[i][j%2] = false;
-    						continue;
-    					} else {
-	    					dp[i][j%2] = false;
-	    					char sChar = s.charAt(i - 1);
-	    					if(sChar == pChar || pChar == '?') {
-	        					dp[i][j%2] = dp[(i - 1)][(j - 1)%2];
-	        				}
-    					}
-    				}
-    			} else {
-    				int k = 0;
-    				for( k = 0; k <= sLen; k++) {
-    					if(dp[k][(j - 1)%2]) {
-    						break;
-    					} else {
-    						dp[k][j%2] = false;
-    					}
-    				}
-    				while(k <= sLen) {
-    					dp[k][j%2] = true;
-    					k++;
-    				}
-    			}
-    		}
-    		res = dp[sLen][pLen%2];
-    	}
-    	return res;
+        if(s == null || p == null) {
+            res = false;
+        } else {
+            int sLen = s.length(), pLen = p.length();
+            boolean [][] dp = new boolean[sLen + 1][pLen + 1];
+            dp[0][0] = true;
+            for(int j = 1; j <= pLen; j++) {
+                char pChar = p.charAt(j - 1);
+                if(pChar != '*') {
+                    for(int i = 1; i <= sLen; i++) {
+                        char sChar = s.charAt(i - 1);
+                        if(sChar == pChar || pChar == '?') {
+                            dp[i][j] = dp[i - 1][j - 1];
+                        }
+                    }
+                } else {
+                    dp[0][j] = dp[0][j - 1];
+                    int k = 0;
+                    while(!dp[k][j - 1]) {
+                        k++;
+                    }
+                    while(k <= sLen) {
+                        dp[k][j] = true;
+                    }
+                }
+            }
+            res = dp[sLen][pLen];
+        }
+        return res;
     }
 }
