f0791d8f6ec2343d45ac2ca10e61d4159fd753de
zsll <zsll@users.noreply.github.com>
2016-10-01 20:01:13 -0700

    [314. Binary Tree Vertical Order Traversal][Accepted]committed by LeetCode Extension

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -10,38 +10,35 @@
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {  
     	List<List<Integer>> res = new ArrayList<>();
-	if(root == null) return res;
-	
-	Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-	Queue<TreeNode> q = new LinkedList<>();
-	Queue<Integer> cols = new LinkedList<>();
-
-	q.add(root); 
-	cols.add(0);
-
-	int min = 0, max = 0;
-	while(!q.isEmpty()) {
-		TreeNode node = q.poll();
-		int col = cols.poll();
-		if(!map.containsKey(col)) map.put(col, new ArrayList<Integer>());
-		map.get(col).add(node.val);
-
-		if(node.left != null) {
-			q.add(node.left); 
-			cols.add(col - 1);
-			if(col <= min) min = col - 1;
-		}
-		if(node.right != null) {
-			q.add(node.right);
-			cols.add(col + 1);
-			if(col >= max) max = col + 1;
-		}
-	}
-
-	for(int i = min; i <= max; i++) {
-		res.add(map.get(i));
-	}
-
-	return res;
+    	if(root != null) {
+	    Queue<TreeNode> q = new LinkedList<TreeNode>();
+	    Queue<Integer> q2 = new LinkedList<Integer>();
+	    q.offer(root);
+	    q2.offer(0);
+	    HashMap<Integer, List<Integer>> h = new HashMap<Integer, List<Integer>>();
+	    int maxL = 0, minL = 0;
+	    while(!q.isEmpty()) {
+	        TreeNode n = q.poll();
+	        int level = q2.poll();
+	        if(!h.containsKey(level)) {
+	            h.put(level, new ArrayList<Integer>());
+	            maxL = Math.max(maxL, level);
+	            minL = Math.min(minL, level);
+	        }
+	        h.get(level).add(n.val);
+	        if(n.left != null) {
+	            q.offer(n.left);
+	            q2.offer(level - 1);
+	        }
+	        if(n.right != null) {
+	            q.offer(n.right);
+	            q2.offer(level + 1);
+	        }
+	    }
+	    for(int i = minL; i <= maxL; i++) {
+	        res.add(h.get(i));
+	    }
+}
+	    return res;
     }
 }
