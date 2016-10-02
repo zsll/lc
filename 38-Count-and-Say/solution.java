fdf42086685b8b1bb71cb961cb4e86b73c9b5a3a
zsll <zsll@users.noreply.github.com>
2016-10-01 21:45:05 -0700

    [38. Count and Say][Accepted]committed by LeetCode Extension

---
+++ 38-Count-and-Say/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String countAndSay(int n) {
+        if(n == 1) {
+            return "1";
+        } else {
+            String pre = countAndSay(n - 1);
+            StringBuffer sb = new StringBuffer();
+            int i = 0, cnt = 0;
+            char c = 0;
+            while(i < pre.length()) {
+                if(i == 0 || pre.charAt(i) != c) {
+                    if(c != 0) {
+                        sb.append(cnt).append(c);
+                    }
+                    cnt = 1;
+                    c = pre.charAt(i);
+                } else {
+                    cnt++;
+                }
+                i++;
+            }
+            sb.append(cnt).append(c);
+            return sb.toString();
+        }
+    }
+}
