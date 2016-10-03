74e72ba127b2fcc03996958c65952aa0362813e4
zsll <zsll@users.noreply.github.com>
2016-10-02 17:53:33 -0700

    [334. Increasing Triplet Subsequence][Accepted]committed by LeetCode Extension

--- 334-Increasing-Triplet-Subsequence/solution.java
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
         boolean res = false;
-        if(nums != null && nums.length >= 3) {
-            int min = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, minRight = Integer.MAX_VALUE;
+        if(nums != null && nums.length > 2) {
+            int rightMin = Integer.MAX_VALUE, min = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
             for(int i : nums) {
                 if(i > min2) {
                     res = true;
                     break;
-                } else if (i > min) {
+                } else if(i > min) {
                     min2 = i;
-                } else if (i > minRight) {
-                    min = minRight;
+                } else if(i > rightMin) {
+                    min = rightMin;
+                    rightMin = Integer.MAX_VALUE;
                     min2 = i;
-                    minRight = Integer.MAX_VALUE;
-                } else {
-                    minRight = i;   //first step
+                } else  {
+                    rightMin = i;
                 }
             }
         }
