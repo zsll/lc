82a9a90bd6403da6ec44159cc0527d35895289dd
zsll <zsll@users.noreply.github.com>
2016-10-02 22:24:22 -0700

    [117. Populating Next Right Pointers in Each Node II][Accepted]committed by LeetCode Extension

--- 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
@@ -8,32 +8,42 @@
  */
 public class Solution {
     public void connect(TreeLinkNode root) {
-        if (root == null) return;  
-      
-    //如果右孩子不为空，左孩子的next是右孩子。  
-    //反之，找root next的至少有一个孩子不为空的节点  
-    if (root.left != null) {  
-        if (root.right != null) {  
-            root.left.next = root.right;  
-        }  
-        else {  
-            TreeLinkNode p = root.next;  
-            while (p != null && p.left == null && p.right == null)  
-                p = p.next;  
-            if (p != null)  
-                root.left.next = p.left == null ? p.right : p.left;  
-        }  
-    }  
-      
-    //右孩子的next 根节点至少有一个孩子不为空的next  
-    if (root.right != null) {  
-        TreeLinkNode p = root.next;  
-        while (p != null && p.left == null && p.right == null)  
-            p = p.next;  
-        if (p != null)  
-            root.right.next = p.left == null ? p.right : p.left;  
-    }  
-    connect(root.right);      
-    connect(root.left); 
+        if(root != null) {
+            if(root.left != null) {
+                if(root.right != null) {
+                    root.left.next = root.right;
+                } else {
+                    TreeLinkNode next = root.next;
+                    while(next != null) {
+                        if(next.left != null) {
+                            root.left.next = next.left;
+                            break;
+                        } else if(next.right != null) {
+                            root.left.next = next.right;
+                            break;
+                        } else {
+                            next = next.next;
+                        }
+                    }
+                }
+            }
+            if(root.right != null) {
+                TreeLinkNode next = root.next;
+                while(next != null) {
+                    if(next.left != null) {
+                        root.right.next = next.left;
+                        break;
+                    } else if(next.right != null) {
+                        root.right.next = next.right;
+                        break;
+                    } else {
+                        next = next.next;
+                    }
+                }
+            }
+            
+            connect(root.right);
+            connect(root.left);
+        }
     }
 }
