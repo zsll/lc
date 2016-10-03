1ef8aa97e20363f2997ad41c95ff52ba46a4ed19
zsll <zsll@users.noreply.github.com>
2016-10-02 21:15:42 -0700

    [286. Walls and Gates][Time Limit Exceeded]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,52 +1,50 @@
 public class Solution {
     public void wallsAndGates(int[][] rooms) {
-	        if(rooms.length == 0) return;
-	        for(int i = 0; i < rooms.length; i++){
-	            for(int j = 0; j < rooms[0].length; j++){
-	                // 如果遇到一个门，从门开始广度优先搜索，标记连通的节点到自己的距离
-	                if(rooms[i][j] == 0) bfs(rooms, i, j);
-	            }
-	        }
-	    }
-	    
-	    public void bfs(int[][] rooms, int i, int j){
-	        Queue<Integer> queue = new LinkedList<Integer>();
-	        queue.offer(i * rooms[0].length + j);
-	        int dist = 0;
-	        // 用一个集合记录已经访问过的点
-	        Set<Integer> visited = new HashSet<Integer>();
-	        visited.add(i * rooms[0].length + j);	//visited storeds visited GATES
-	        while(!queue.isEmpty()){
-	            int size = queue.size();
-	            // 记录深度的搜索
-	            for(int k = 0; k < size; k++){
-	                Integer curr = queue.poll();
-	                int row = curr / rooms[0].length;
-	                int col = curr % rooms[0].length;
-	                // 选取之前标记的值和当前的距离的较小值
-	                rooms[row][col] = Math.min(rooms[row][col], dist);
-	                int up = (row - 1) * rooms[0].length + col;
-	                int down = (row + 1) * rooms[0].length + col;
-	                int left = row * rooms[0].length + col - 1;
-	                int right = row * rooms[0].length + col + 1;
-	                if(row > 0 && rooms[row - 1][col] > 0 && !visited.contains(up)){
-	                    queue.offer(up);
-	                    visited.add(up);
+        if(rooms != null && rooms.length > 0 && rooms[0].length > 0) {
+            int h = rooms.length, w = rooms[0].length;
+            for(int i = 0; i < h; i++) {
+                for(int j = 0; j < w; j++) {
+                    
+                    if(rooms[i][j] == 0) {
+                        bfs(rooms, i, j);
+                    }
+                }
+            }
+        }
+    }
+    
+    void bfs(int[][] rooms, int i, int j) {
+            Queue<Integer> xq = new LinkedList<Integer>();
+	            Queue<Integer> yq = new LinkedList<Integer>();
+	            Queue<Integer> preDist = new LinkedList<Integer>();	//You may use level by level traversal, or q queue
+	            xq.offer(i);
+	            yq.offer(j);
+	            preDist.offer(0);
+	            int h = rooms.length, w = rooms[0].length;
+	            boolean [][] v = new boolean[h][w];
+	            v[i][j] = true;
+	            while(!xq.isEmpty()) {
+	                int curX = xq.poll(), curY = yq.poll();
+	                int dist = preDist.poll();
+	                rooms[curX][curY] = Math.min(rooms[curX][curY], dist);
+	                    
+	                int [][] dir = {
+	                    {0, 1},
+	                    {1, 0},
+	                    {0, -1},
+	                    {-1, 0}
+	                };
+	                for(int k = 0; k < 4; k++) {
+	                    int x = curX + dir[k][0], y = curY + dir[k][1];
+	                    if(x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && !v[x][y] && rooms[x][y] >= 0) {
+	                        xq.offer(x);
+	                        yq.offer(y);
+	                        preDist.offer(dist + 1);
+	                        v[x][y] = true;
+	                    }
 	                }
-	                if(col > 0 && rooms[row][col - 1] > 0 && !visited.contains(left)){
-	                    queue.offer(left);
-	                    visited.add(left);
-	                }
-	                if(row < rooms.length - 1 && rooms[row + 1][col] > 0 && !visited.contains(down)){
-	                    queue.offer(down);
-	                    visited.add(down);
-	                }
-	                if(col < rooms[0].length - 1 && rooms[row][col + 1] > 0 && !visited.contains(right)){
-	                    queue.offer(right);
-	                    visited.add(right);
-	                }
-	            }
-	            dist++;
-	        }
-	    }
+	                dist++;
+            }
+        
+    }
 }
