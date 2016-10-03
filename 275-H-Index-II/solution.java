bb13202433f36ab241c56e813eacc1ac1bc24403
zsll <zsll@users.noreply.github.com>
2016-10-02 17:43:13 -0700

    [275. H-Index II][Accepted]committed by LeetCode Extension

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -1,41 +1,29 @@
 public class Solution {
     public int hIndex(int[] citations) {
- int r = 0;
-    	//if(citations != null && citations.length > 0) {
-    		int l = citations.length;
-    		int start = 0, end = l - 1;
-    		//To find first max article number so that article number is smaller than or equal to citation:
-    		/**
-    		 *Note that when hIndexAscendingOrder, article number that is larger than
-    		 *or equal to i is in descending order
-    		 *The citations is ascending
-0 1 3 5 6
-While the article number is descending
-The articles larger than or equal to 0 (c[i]) is 5, 
-larger than or equal to 1 (c[i]) is 4,
-larger than or equal to 3 is 3, 
-larger than or equal to 5 is 2,
-larger than or equal to 6 is 1
-
-Note that the article number is descending, so there will be a intersection to make sure that citation number is larger than
-or equal to article number, and we need to find the max of it
-We can let left equals 5 (corresponding index is length 5 minus left), right equals 1 (corresponding index is length 5 minus right)
-So when getting mid, we know the article number that is larger than ci[mid] is (5 minus mid)
-if ci[mid] >= 5 minus mid, it's a valid candidate
-We can try end equals mid minus 1
-    		 */
-    		while(start <= end) {
-    			int mid = (start + end)/2;
-    			int articleNum = l - mid;
-    			if(articleNum > citations[mid]) {	//This is not a valid h index, and valid h is on right
-
-    				start = mid + 1;	//for left is not possible
-    			} else {	//articleNum <= citations[mid]  articleNum is valid h, will need to find larger article, which is on left
-    	    		//r = articleNum;
-    				end = mid - 1;
-    			}
-    		}
-    	//}
-    	return l - start;
+        int res = 0;
+        if(citations != null && citations.length > 0) {
+            int start = 0, end = citations.length - 1;
+            while(start <= end) {
+                int mid = start + (end - start >>1);
+                int articleNumberThatHasMoreCi = citations.length - mid;    //how many articles has more ci than citations[mid]
+                /*
+                "A scientist has index h if h of his/her N papers have at least h citations each, 
+and the other N − h papers have no more than h citations each."
+                */
+                if(articleNumberThatHasMoreCi > citations[mid]) {  //invalid case, see example below
+                    start = mid + 1;
+                } else {    //articleNumberThatHasMoreCi >= citations[mid]
+                    end = mid - 1;
+                }
+                res = citations.length - start;
+            }
+        }
+        return res;
     }
+    
+     /**
+      * 1 1 2 100
+      * 
+      * answer should be 2
+      */
 }
