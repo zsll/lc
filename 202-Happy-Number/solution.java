83e7cda5e02afb1af1491d613b75cd256ccd5c6f
zsll <zsll@users.noreply.github.com>
2016-10-04 01:21:47 -0700

    [202. Happy Number][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 202-Happy-Number/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean isHappy(int n) {
+        boolean res = true;
+        HashSet<Integer> s = new HashSet<Integer>();
+        while(n != 1) {
+            if(s.contains(n)) {
+                res = false;
+                break;
+            }
+            n = sumDigit(n);
+        }
+        return res;
+    }
+    
+    int sumDigit(int n) {
+        int res = 0;
+        while(n > 0) {
+            int digit = n%10;
+            n /= 10;
+            res += digit*digit;
+        }
+        return res;
+    }
+}
