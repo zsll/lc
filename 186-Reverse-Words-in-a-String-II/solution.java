b4c0a64edf0ae44786de6a49d4378abd9ed7861e
zsll <zsll@users.noreply.github.com>
2016-10-04 00:30:33 -0700

    [186. Reverse Words in a String II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 186-Reverse-Words-in-a-String-II/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public void reverseWords(char[] s) {
+        if(s != null && s.length > 1) {
+            reverse(s, 0, s.length - 1);
+            int start = 0, end = 0;
+            while(end < s.length) {
+                start = findStart(s, end);
+                end = findStart(s, start);
+                if(end < s.length)
+                    reverse(s, start, end);
+            }
+        }
+    }
+    
+    void reverse(char [] s, int start, int end) {
+        while(start < end) {
+            char c = s[start];
+            s[start] = s[end];
+            s[end] = c;
+        }
+    }
+    
+    int findStart(char [] s, int start) {
+        while(start < s.length && !(start == 0 && s[start] != ' ' || s[start] != ' ' && s[start - 1] == ' ')) {
+            start++;
+        }
+        return start;
+    }
+    
+    
+    int findEnd(char [] s, int start) {
+        while(start < s.length && !(start == s.length - 1 && s[start] != ' ' || s[start] != ' ' && s[start + 1] == ' ')) {
+            start++;
+        }
+        return start;
+    }
+}
