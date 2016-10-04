f8983f8d18adcedd2d840c26aa75f085b96e34ee
zsll <zsll@users.noreply.github.com>
2016-10-04 14:35:48 -0700

    [373. Find K Pairs with Smallest Sums][Accepted]committed by LeetCode Extension

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,54 +1,43 @@
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<int[]> res = new ArrayList<int[]>();
-        if(nums1 != null && nums1.length > 0 && nums2 != null && nums2.length > 0 && k > 0) {
-            PriorityQueue<PosVal2> q = new PriorityQueue<PosVal2>(10, new Comparator<PosVal2>() {
-                @Override
-                public int compare(PosVal2 a, PosVal2 b) {
-                    return Integer.compare(a.val, b.val);
-                }
+        if(nums1 != null && nums2 != null && nums1.length > 0 && nums2.length > 0 && k > 0) {
+            int len1 = nums1.length, len2 = nums2.length;
+            PriorityQueue<PosVal> pq = new PriorityQueue<PosVal>(k, new Comparator<PosVal> () {
+                 public int compare(PosVal a, PosVal b) {
+                     return a.v - b.v;
+                 }
+                
             });
-            HashSet<Integer> visited = new HashSet<Integer>();
-            PosVal2 cur = new PosVal2(0, 0, nums1[0] + nums2[0]);
-            q.offer(cur);
-            int width = nums2.length;
-            visited.add(encodePos(0,0,width));
-            for(int i = 0; i < k; i++) {
-                PosVal2 min = q.poll();
-                if(min == null) {
-                    break;  //NoteNote
-                }
-                int[] t = new int[2];
-                t[0] = nums1[min.row];
-                t[1] = nums2[min.col];
+            HashSet<Integer> v = new HashSet<Integer>();
+            PosVal pv = new PosVal(0, 0, nums1, nums2);
+            v.add(0);
+            pq.offer(pv);
+            int count = 0;
+            while(pq.size() > 0 && count < k) {
+                pv = pq.poll();
+                int [] t = {nums1[pv.x], nums2[pv.y]};
                 res.add(t);
-                if(min.row + 1 < nums1.length && !visited.contains(encodePos(min.row + 1, min.col, width))) {
-                    cur = new PosVal2(min.row + 1, min.col, nums1[min.row + 1] + nums2[min.col]);
-                    q.offer(cur);
-                    visited.add(encodePos(min.row + 1, min.col, width));
+                if(pv.x + 1 < len1 && !v.contains((pv.x + 1) * len2 + pv.y)) {
+                    pq.offer(new PosVal(pv.x + 1, pv.y, nums1, nums2));
+                    v.add((pv.x + 1) * len2 + pv.y);
                 }
-                if(min.col + 1 < nums2.length && !visited.contains(encodePos(min.row , min.col + 1, width))) {
-                    cur = new PosVal2(min.row , min.col + 1, nums1[min.row ] + nums2[min.col + 1]);
-                    q.offer(cur);
-                    visited.add(encodePos(min.row, min.col + 1, width));
+                if(pv.y + 1 < len2 && !v.contains((pv.x) * len2 + pv.y + 1)) {
+                    pq.offer(new PosVal(pv.x, pv.y + 1, nums1, nums2));
+                    v.add((pv.x) * len2 + pv.y + 1);
                 }
+                count++;
             }
         }
         return res;
     }
     
-    int encodePos(int row, int col, int width) {
-        return row*width + col;
-    }
-    
-    class PosVal2 {
-        int row, col, val;
-        public PosVal2(int i, int j, int val) {
-            this.row = i;
-            this.col = j;
-            this.val = val;
+    class PosVal {
+        int x, y, v;
+        public PosVal(int x, int y, int [] nums1, int [] nums2) {
+            this.x = x;
+            this.y = y;
+            this.v = nums1[x] + nums2[y];
         }
-        
-        
     }
 }
