6f5d83d5429e23f14bf326ce77e9d9424141b562
zsll <zsll@users.noreply.github.com>
2016-10-04 15:36:16 -0700

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.java
+++ 205-Isomorphic-Strings/solution.java
@@ -1,25 +1,18 @@
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        boolean res = true;
-        if(s == null && t != null || t == null && s != null) {
-            res = false;
-        } else if (s != null && t != null) {
-            if(s.length() != t.length()) {
-                res = false;
-            } else {
-                int [] st = new int[257];    //if not set, will be 0
-                int [] ts = new int[257];
-                for(int i = 0; i < s.length(); i++) {
-                    int sc = (int)(s.charAt(i)  + 1);  //1 to 256
-                    int tc = (int)(t.charAt(i)  + 1);
-                    if(st[sc] == 0 && ts[tc] == 0) {
-                        st[sc] = tc;
-                        ts[tc] = sc;
-                    } else if  ( (st[sc] > 0 && (ts[tc] == 0 || st[sc] != tc) ) 
-                    ||  (ts[tc] > 0 && (st[sc] == 0 || ts[tc] != sc ) )){
-                        res = false;
-                        break;
-                    }
+        boolean res = false;
+        if(s != null && t != null && s.length() == t.length()) {
+            res = true;
+            HashMap<Character, Character> aToB = new HashMap<Character, Character>();   //no mutual
+            HashMap<Character, Character> bToA = new HashMap<Character, Character>();   //no mutual
+            for(int i = 0; i < s.length(); i++) {
+                char sChar = s.charAt(i), tChar = t.charAt(i);
+                if(! (aToB.containsKey(sChar) && aToB.get(sChar) != tChar || bToA.containsKey(tChar) && bToA.get(tChar) != sChar ) ){
+                    aToB.put(sChar, tChar);
+                    bToA.put(tChar, sChar);
+                } else {
+                    res = false;
+                    break;
                 }
             }
         }
