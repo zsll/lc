e2aac9b9e85fffcfdedd3aaa35436c27bc132404
zsll <zsll@users.noreply.github.com>
2016-10-04 01:13:37 -0700

    [242. Valid Anagram][Accepted]committed by LeetCode Extension

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,22 +1,17 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        boolean res = true;
-        if(s == null && t != null || s != null && t == null) {
-            res = false;
-        } else if (s != null && t != null) {
-            if(s.length() != t.length()) {
-                res = false;
-            } else {
-                int [] sh = new int[256];
-                int [] th = new int[256];
-                for(int i = 0; i < s.length(); i++) {
-                    sh[(int)s.charAt(i)]++;
-                    th[(int)t.charAt(i)]++;
-                }
-                for(int i = 0; i < s.length(); i++) {
-                    if(sh[(int)s.charAt(i)] != th[(int)s.charAt(i)]) {
-                        res = false;
-                    }
+        boolean res = false;
+        if(s != null && t != null && s.length() == t.length()) {
+            int [] h = new int[255];
+            for(int i = 0; i < s.length(); i++) {
+                h[s.charAt(i)]++;
+                h[t.charAt(i)]--;
+            }
+            res = true;
+            for(int i = 0; i < s.length(); i++) {
+                if(h[s.charAt(i)] != 0) {
+                    res = false;
+                    break;
                 }
             }
         }
