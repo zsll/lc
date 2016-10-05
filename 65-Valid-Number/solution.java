c542314894b13dfdbc46fb494a8c6918f8f18b3d
zsll <zsll@users.noreply.github.com>
2016-10-04 23:56:37 -0700

    [65. Valid Number][Accepted]committed by LeetCode Extension

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
+    
     public boolean isNumber(String s) {
-        String pattern = "\\s*[+-]?((\\d+\\.?\\d*)|\\.\\d+)(e[+-]?\\d+)?\\s*";
-        
+        String pattern = "\\s*[+-]?(\\d*\\.?\\d+|\\d+\\.?\\d*)(e[+-]?\\d+)?\\s*";  //NoteNote, . needs to be escaped using \\
         return s.matches(pattern);
     }
 }
