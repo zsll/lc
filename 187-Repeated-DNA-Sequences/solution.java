b935fb2b81cc96705a016679bb2b71ac2474fccf
zsll <zsll@users.noreply.github.com>
2016-10-05 10:37:28 -0700

    [187. Repeated DNA Sequences][Accepted]committed by LeetCode Extension

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,36 +1,38 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> res = new ArrayList<String>();
-        
-        if(s == null || s.length() < 10) {
-            return res;
-        }
-        HashMap<Character, Integer> d = new HashMap<Character, Integer>();
-        d.put('A', 0);
-        d.put('C', 1);
-        d.put('G', 2);
-        d.put('T', 3);
-        HashSet<Integer> m = new HashSet<Integer>();
-        int k = 0;
-        for(int i = 0; i < 10; i++) {
-            k = (k<<2) + d.get(s.charAt(i));    //NoteNote, needs ()
-        }
-        m.add(k);
-        for(int i = 10; i < s.length(); i++) {
-            k = ((k<<2) & 0xFFFFF) + d.get(s.charAt(i));
+        if(s != null && s.length() >= 10) {
+            HashMap<Character, Integer> m = new HashMap<Character, Integer>();
             
-            if(!m.add(k) && res.indexOf(s.substring(i - 9, i + 1)) == -1) {
-                /**
-                 * Input:
+            HashMap<Integer, String> h = new HashMap<Integer, String>();
+            m.put('A', 0);
+            m.put('C', 1);
+            m.put('T', 2);
+            m.put('G', 3);
+            int sum = 0;
+            for(int i = 0; i < 10; i++) {
+                sum = (sum<<2) | m.get(s.charAt(i));
+            }
+            String sub = s.substring(0, 10);
+            h.put(sum, sub);
+            for(int i = 10; i < s.length(); i++) {
+                sum = (sum<<2)&0xFFFFF | m.get(s.charAt(i));
+                sub = s.substring(i - 10 + 1, i + 1);
+                if(!h.containsKey(sum)) {
+                    h.put(sum, sub);
+                } else if(res.indexOf(sub) == -1) {
+                    /**
+                     * Submission Result: Wrong Answer More Details 
+
+Input:
 "AAAAAAAAAAAA"
 Output:
 ["AAAAAAAAAA","AAAAAAAAAA"]
 Expected:
-["AAAAAAAAAA"]
-*/
-                res.add(s.substring(i - 9, i + 1));
-            } 
-            
+["AAAAAAAAAA"]*/
+                    res.add(sub);
+                }
+            }
         }
         return res;
     }
