07c0f324b8e136cb5bd96be871935888049b7624
zsll <zsll@users.noreply.github.com>
2016-10-05 10:49:22 -0700

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int maxProduct(int[] nums) {
-        int res = Integer.MIN_VALUE;
+        int res = 0;
         if(nums != null && nums.length > 0) {
             int max = nums[0], min = nums[0];
-            res = max;
+            res = nums[0];
             for(int i = 1; i < nums.length; i++) {
                 int preMax = max;
-                max = Math.max(Math.max(nums[i], max*nums[i]), min*nums[i]);
-                min = Math.min(Math.min(nums[i], min*nums[i]), preMax*nums[i]);
+                max = Math.max(nums[i], Math.max(nums[i] * max, nums[i] * min));
+                min = Math.min(nums[i], Math.min(nums[i] * preMax, nums[i] * min));
                 res = Math.max(res, max);
             }
         }
