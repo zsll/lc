83f3cf4e515ee153c531222dfe45cb7354581658
zsll <zsll@users.noreply.github.com>
2016-10-05 08:38:12 -0700

    [149. Max Points on a Line][Accepted]committed by LeetCode Extension

--- 149-Max-Points-on-a-Line/solution.java
+++ 149-Max-Points-on-a-Line/solution.java
@@ -12,29 +12,30 @@ public class Solution {
         int res = 0;
         if(points != null && points.length > 0) {
             res = 1;
-            int len = points.length;
-            for(int i = 0; i < len - 1; i++) {
-                double k = Double.MIN_VALUE;
-                int maxLinesThroughI = 0,//not including self, all the lines through i with same k (slope)
-                count = 1;  //self
-                HashMap<Double, Integer> map = new HashMap<Double, Integer>();
-                for(int j = i + 1; j < len; j++) {
-                    if(points[j].x == points[i].x && points[j].y == points[i].y) {
-                        count++;
-                    } else {
-                        k = Double.MAX_VALUE;
-                        if(points[j].x - points[i].x != 0) {
-                            k = 1.0 *(points[j].y - points[i].y)/(points[j].x - points[i].x) + 0.0;
+            if(points.length > 1) {
+                for(int i = 0; i < points.length; i++) {
+                    HashMap<Double, Integer> h = new HashMap<Double, Integer>();
+                    int dup = 1;//self
+                    for(int j = 0; j < points.length; j++) {
+                        if(i != j) {
+                            if(points[i].y == points[j].y && points[i].x == points[j].x) {
+                                dup++;
+                                res = Math.max(dup, res);
+                            } else {
+                                Double k = Double.MAX_VALUE;
+                                if(points[i].x != points[j].x) {
+                                    k = 1.0*(points[i].y - points[j].y)/(points[i].x - points[j].x);//Line 27: error: incompatible types: int cannot be converted to Double
+                                }
+                                if(!h.containsKey(k)) {
+                                    h.put(k, 0);
+                                }
+                                int pre = h.get(k);
+                                h.put(k, pre + 1);
+                                res = Math.max(dup + h.get(k), res);
+                            }
                         }
-                        if(!map.containsKey(k)) {
-                            map.put(k, 0);
-                        }
-                        map.put(k, map.get(k) + 1);
-                        
-                        maxLinesThroughI = Math.max(map.get(k), maxLinesThroughI);
                     }
                 }
-                res = Math.max(count + maxLinesThroughI, res);
             }
         }
         return res;
