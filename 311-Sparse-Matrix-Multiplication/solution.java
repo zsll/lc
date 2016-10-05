3a91933c9c1dce36e564741f57e7b7bc566d17db
zsll <zsll@users.noreply.github.com>
2016-10-05 11:51:13 -0700

    [311. Sparse Matrix Multiplication][Accepted]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -3,14 +3,35 @@ public class Solution {
         if(A == null || B == null || A.length == 0 || B.length == 0 || A[0].length == 0 || B[0].length == 0) {
             return null;
         }
-        int h = A.length, w = B[0].length, p = B.length;
+        int h = A.length, w = B[0].length;
         int [][] res = new int[h][w];
+        
+        TreeMap<Integer, TreeMap<Integer, Integer>> a = getHash(A);
+        TreeMap<Integer, TreeMap<Integer, Integer>> b = getHash(B);
         for(int i = 0; i < h; i++) {
-            for(int j = 0; j < p; j++) {
-                if(A[i][j] != 0) {  //NoteNote
-                    for(int k = 0; k < w; k++) {
-                        res[i][k] += A[i][j]*B[j][k];
+            for(int j = 0; j < w; j++) {
+                if(a.containsKey(i)) {
+                    for(int nonZeroCol : a.get(i).keySet()) {
+                        if(b.containsKey(nonZeroCol) && b.get(nonZeroCol).containsKey(j)) {
+                            res[i][j] += a.get(i).get(nonZeroCol)*b.get(nonZeroCol).get(j);
+                        }
                     }
+                }
+            }
+        }
+        return res;
+    }
+    
+    TreeMap<Integer, TreeMap<Integer, Integer>> getHash(int [][] m) {
+        TreeMap<Integer, TreeMap<Integer, Integer>> res = new TreeMap<Integer, TreeMap<Integer, Integer>>();
+        int h = m.length, w = m[0].length;
+        for(int i = 0; i < h; i++) {
+            for(int j = 0; j < w; j++) {
+                if(m[i][j] != 0) {
+                    if(!res.containsKey(i)) {
+                        res.put(i, new TreeMap<Integer, Integer>());
+                    }
+                    res.get(i).put(j, m[i][j]);
                 }
             }
         }
