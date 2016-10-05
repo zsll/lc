a316a359867c63aed8b18208f1f95e233902ebf0
zsll <zsll@users.noreply.github.com>
2016-10-05 15:13:32 -0700

    [57. Insert Interval][Accepted]committed by LeetCode Extension

--- 57-Insert-Interval/solution.java
+++ 57-Insert-Interval/solution.java
@@ -9,19 +9,21 @@
  */
 public class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-         List<Interval> res = new ArrayList<Interval>();
-         for(Interval i : intervals) {
-             if(newInterval.end < i.start) {
-                 res.add(newInterval);
-                 newInterval = i;
-             } else if (newInterval.start > i.end) {
-                 res.add(i);
-             } else {
-                 newInterval.start = Math.min(newInterval.start, i.start);
-                 newInterval.end = Math.max(newInterval.end, i.end);
-             }
-         }
-         res.add(newInterval);
-         return res;
+        List<Interval> res = new ArrayList<Interval>();
+        if(intervals != null) {
+            for(Interval i : intervals) {
+                if(newInterval.end < i.start) {
+                    res.add(newInterval);
+                    newInterval = i;
+                } else if(i.end < newInterval.start) {
+                    res.add(i);
+                 }  else {
+                     newInterval.start = Math.min(newInterval.start, i.start);
+                    newInterval.end = Math.max(newInterval.end, i.end);
+                }
+            }
+            res.add(newInterval);
+        }
+        return res;
     }
 }
