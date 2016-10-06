f6d80af3b0bb4a8a712e02b013142e9f048a7258
zsll <zsll@users.noreply.github.com>
2016-10-05 21:11:47 -0700

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         boolean res = false;
-        if(nums != null && nums.length > 0 && k >= 0 && t >= 0) {
-            SortedSet<Long> set = new TreeSet<Long>();
-            for(int i = 0; i < nums.length; i++) {
-                long left = (long)nums[i] - t, right = (long)nums[i] + t + 1;
-                SortedSet<Long> sub = set.subSet(left, right);
+        if(nums != null && nums.length > 1 && k > 0 && t >= 0) {
+            SortedSet<Long> m = new TreeSet<Long>();
+            for(int i = 0; i < nums.length ; i++) {
+                SortedSet<Long> sub = m.subSet(((long)nums[i] - t), ((long)nums[i] + t + 1));   //Needs to convert firstly, then +/- t
                 if(sub.size() > 0) {
                     res = true;
                     break;
                 }
-                set.add((long)nums[i]);
+                m.add((long)nums[i]);
                 if(i >= k) {
-                    set.remove((long)nums[i - k]);
+                    m.remove((long)nums[i - k]);
                 }
             }
         }
