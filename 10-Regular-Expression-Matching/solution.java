35a95c1eb3c698b066033ddf45308252e14505b6
zsll <zsll@users.noreply.github.com>
2016-10-05 18:19:45 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,33 +1,33 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int h = s.length(), w = p.length();
-        boolean [][] dp = new boolean[2][w + 1];
-        for(int i = 0; i <= h; i++) {
-            for(int j = 0; j <= w; j++) {
-                dp[i%2][j] = false;
-                if(j == 0 && i == 0) {
-                    dp[i%2][j] = true;
-                } else if (i == 0) {
-                    if(j >= 2 && dp[i][j - 2] && p.charAt(j - 1) == '*' && p.charAt(j - 2) != '*') {
+        boolean res = false;
+        if(s != null && p != null) {
+            int h = s.length(), w = p.length();
+            boolean [][] dp = new boolean[2][w + 1];
+            for(int i = 0; i <= h; i++) {
+                for(int j = 0; j <= w; j++) {
+                    dp[i%2][j] = false;
+                    if(i == 0 && j == 0) {
                         dp[i%2][j] = true;
-                    }
-                } else if (j > 0 ) {    //Note Note
-                    if(p.charAt(j - 1) != '*') {
-                        if(s.charAt(i - 1) == p.charAt(j - 1) ||  p.charAt(j - 1) == '.') {
-                            dp[i%2][j] = dp[(i - 1)%2][j - 1];
-                        }
+                    } else if(i == 0) {
+                        dp[i%2][j] = (j > 1 && p.charAt(j - 1) == '*' && p.charAt(j - 2) != '*' && dp[i%2][j - 2]);
+                    } else if(j == 0) {
+                        dp[i%2][j] = false;
                     } else {
-                        if(j >= 2) {    //NoteNote, else must be false
-                            if(p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.' ) {
-                                dp[i%2][j] = dp[i%2][j - 2] || dp[(i - 1)%2][j];
-                            } else {
+                        if(p.charAt(j - 1) != '*') {
+                            dp[i%2][j] = (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) && dp[(i - 1)%2][j - 1];
+                        } else {
+                            if(j >=2 && (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.' ) ) {
+                                dp[i%2][j] = dp[i%2][j - 2] | dp[(i - 1)%2][j];
+                            } else if(j >= 2) {
                                 dp[i%2][j] = dp[i%2][j - 2];
                             }
                         }
                     }
                 }
             }
+            res = dp[h%2][w];
         }
-        return dp[h%2][w];
+        return res;
     }
 }
