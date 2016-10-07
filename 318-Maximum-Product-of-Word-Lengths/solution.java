83a86ef8d2a5ae4f6f27170efd2237289edb4e11
zsll <zsll@users.noreply.github.com>
2016-10-07 09:26:06 -0700

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -1,18 +1,18 @@
 public class Solution {
     public int maxProduct(String[] words) {
-        //NoteNote since maximum chars a word can contain is 26, can use an integer as hash to represent the chars a word contains
         int res = 0;
         if(words != null && words.length > 0) {
-            int len = words.length;
-            int [] b = new int[len];
-            for(int i = 0; i < len; i++) {
-                for(int j = 0; j < words[i].length(); j++) {
-                    b[i] |= 1 << (words[i].charAt(j) - 'a');
+            HashMap<String, Integer> m = new HashMap<String, Integer>();
+            for(String s : words) {
+                int i = 0;
+                for(char c : s.toCharArray()) {
+                    i = i | (1 << (c - 'a'));
                 }
+                m.put(s, i);
             }
-            for(int i = 0; i < len; i++) {
-                for(int j = i + 1; j < len; j++) {
-                    if((b[i] & b[j]) == 0) {
+            for(int i = 0; i < words.length - 1; i++) {
+                for(int j = i + 1; j < words.length; j++) {
+                    if((m.get(words[i]) & m.get(words[j])) == 0) {//
                         res = Math.max(words[i].length() * words[j].length(), res);
                     }
                 }
