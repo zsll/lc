5b42afff26b30415b4cb72ac654c245af6739b6e
zsll <zsll@users.noreply.github.com>
2016-10-08 13:58:28 -0700

    [359. Logger Rate Limiter][Accepted]committed by LeetCode Extension

---
+++ 359-Logger-Rate-Limiter/solution.java
@@ -0,0 +1,25 @@
+public class Logger {
+    HashMap<String, Integer> m = new HashMap<String, Integer>();
+    /** Initialize your data structure here. */
+    public Logger() {
+        
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!m.containsKey(message) || (timestamp - m.get(message) >= 10)) {
+            m.put(message, timestamp);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
