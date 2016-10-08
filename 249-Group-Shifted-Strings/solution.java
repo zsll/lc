e65f4d028cf7ac0bbac760cf3cc683c36ea1af50
zsll <zsll@users.noreply.github.com>
2016-10-08 14:34:22 -0700

    [249. Group Shifted Strings][Accepted]committed by LeetCode Extension

--- 249-Group-Shifted-Strings/solution.java
+++ 249-Group-Shifted-Strings/solution.java
@@ -1,35 +1,26 @@
 public class Solution {
     public List<List<String>> groupStrings(String[] strings) {
         List<List<String>> res = new ArrayList<List<String>>();
-        if(strings != null && strings.length > 0) {
-            HashMap<String, List<String>> m = new HashMap<String, List<String>>();
-            for(String s : strings) {
-                String k = getKey(s);
-                if(!m.containsKey(k)) {
-                    m.put(k, new ArrayList<String>());
-                }
-                m.get(k).add(s);
+        HashMap<String, List<String>> h = new HashMap<String, List<String>>();
+        for(String s : strings) {
+            String code = encode(s);
+            if(!h.containsKey(code)) {
+                h.put(code, new ArrayList<String>());
             }
-            for(String s : m.keySet()) {
-                res.add(m.get(s));
-            }
+            h.get(code).add(s);
+        }
+        for(Map.Entry<String, List<String>> e : h.entrySet()) {
+            res.add(e.getValue());
         }
         return res;
     }
     
-    String getKey(String s) {
+    String encode(String s) {
         StringBuffer sb = new StringBuffer();
-        if(s != null && s.length() > 0) {
-            for(int i = 1; i < s.length(); i++) {
-                /**
-                 * Output:
-[["abc","bcd","xyz"],["a","z"],["acef"],["az"],["ba"]]
-Expected:
-[["acef"],["a","z"],["abc","bcd","xyz"],["az","ba"]]
-Rotation
-*/
-                sb.append((s.charAt(i) - s.charAt(i - 1) + 26)%26);
-            }
+        for(int i = 1; i < s.length(); i++) {
+            int diff = (s.charAt(i) - s.charAt(i - 1) + 26)%26;
+            sb.append(",");
+            sb.append(diff);
         }
         return sb.toString();
     }
