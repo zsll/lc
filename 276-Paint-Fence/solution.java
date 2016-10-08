f16a36a861e891fc49a5249ed4fd26e406810904
zsll <zsll@users.noreply.github.com>
2016-10-07 17:29:23 -0700

    [276. Paint Fence][Accepted]committed by LeetCode Extension

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -2,15 +2,15 @@ public class Solution {
     public int numWays(int n, int k) {
         int res = 0;
         if(n > 0 && k > 0) {
-            int [] s = new int[n + 1];
-            int [] d = new int[n + 1];
+            int [] s = new int[2];
+            int [] d = new int[2];
             s[0] = 0; s[1] = 0;//NoteNote the initialization
             d[1] = 0; d[1] = k;//NoteNote the initialization
             for(int i = 2; i <= n; i++) {
-                s[i] = d[i - 1];
-                d[i] = (s[i - 1] + d[i - 1])*(k - 1);
+                s[i%2] = d[(i - 1)%2];
+                d[i%2] = (s[(i - 1)%2] + d[(i - 1)%2])*(k - 1);
             }
-            res = s[n] + d[n];
+            res = s[n%2] + d[n%2];
         }
         return res;
     }
