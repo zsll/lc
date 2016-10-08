a690305d1c400c58b6f364533d4719bd5593d2d5
zsll <zsll@users.noreply.github.com>
2016-10-08 01:43:25 -0700

    [324. Wiggle Sort II][Accepted]committed by LeetCode Extension

--- 324-Wiggle-Sort-II/solution.java
+++ 324-Wiggle-Sort-II/solution.java
@@ -1,103 +1,116 @@
 public class Solution {
     public void wiggleSort(int[] nums) {
-        int mid = findKth(nums, (nums.length + 1)/2);
-        threeWaySort(nums, mid);
-        /**
-         * 0 1 2 3 4 5 6
-         * 0 4 1 5 2 6 3
-         * 
-         * 0 -> 0
-         * 1 -> 2
-         * 2 -> 4
-         * 3 -> 6
-         * 
-         * So new index = i*2
-         * 
-         * 4 -> 1
-         * 5 -> 3
-         * 6 -> 5
-         * len - new = 2*(len - i)
-         * new = len - 2*(len - i) = 2*i - len
-         */ 
-         remap(nums);
-    }
-    
-    void swap(int [] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    void remap(int [] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > 0) {   //So it works for positive numbers
-                int newI = getNewI(nums, i);
-                while(i != newI) {
-                    swap(nums, i, newI);
-                    nums[newI] = 0 - nums[newI];
-                    newI = getNewI(nums, newI);
-                }
-                nums[i] = 0 - nums[i];
-            }
-        }
-    }
-    
-    int getNewI(int [] nums, int i) {
-        if(i <= (nums.length - 1)/2) {
-            return i*2;
-        } else {
-            return 2*i - nums.length;
-        }
-    }
-    
-    void threeWaySort(int [] nums, int mid) {
-        int zEnd = 0, tStart = nums.length - 1;
-        for(int i = 0; i <= tStart; i++) {
-            if(nums[i] < mid) {
-                swap(nums, zEnd, i);
-                zEnd++;
-            } else if(nums[i] > mid) {
-                swap(nums, i, tStart);
-                tStart--;
-                i--;
-            }
-        }
-    }
-    
-    int findKth(int [] nums, int k) {
-        int start = 0, end = nums.length - 1;
-        while(start <= end) {
-            int m = partition(nums, start, end);
-            if(m == k - 1) {
-                return nums[m];
-            } else if (m > k - 1) {
-                end = m - 1;
-            } else {
-                start = m + 1;
-            }
-        }
-        return Integer.MAX_VALUE;
-    }
-    
-    int partition(int [] nums, int start, int end) {
-        int t = nums[start];
-        while(start < end) {
-            while(end > start && nums[end] >= t) {
-                end--;
-            }
-            if(end > start) {
-                nums[start] = nums[end];
-                start++;
-            }
-            while(end > start && nums[start] <= t) {
-                start++;
-            }
-            if(end > start) {
-                nums[end] = nums[end];
-                end--;
-            }
-        }
-        nums[start] = t;
-        return start;
-    }
+        int mid = findKth2(nums, (nums.length + 1)/2);
+	        threeWaySort(nums, mid);
+	        /**
+	         * 0 1 2 3 4 5 6
+	         * 0 4 1 5 2 6 3
+	         * 
+	         * 0 -> 0
+	         * 1 -> 2
+	         * 2 -> 4
+	         * 3 -> 6
+	         * 
+	         * So new index = i*2
+	         * len = 7
+	         * 4 -> 1
+	         * 5 -> 3
+	         * 6 -> 5
+	         * len - new = 2*(len - i)
+	         * new = len - 2*(len - i) = 2*i - len
+	         * 
+	         * NoteNote, the mapping for second half differs for odd/even length
+	         * 0 1 2 3 4 5
+	         * 0 3 1 4 2 5
+	         * len = 6
+	         * 3 -> 1
+	         * 4 -> 3
+	         * 5 -> 5
+	         * new = i*2 -len + 1
+	         */ 
+	         remap(nums);
+	    }
+	    
+	    
+	    void remap(int [] nums) {
+	        for(int i = 0; i < nums.length; i++) {
+	            if(nums[i] > 0) {   //So it works for positive numbers
+	                int newI = getNewI(nums, i);
+	                while(i != newI) {
+	                    swap(nums, i, newI);
+	                    nums[newI] = 0 - nums[newI];
+	                    newI = getNewI(nums, newI);
+	                }
+	                nums[i] = 0 - nums[i];
+	            }
+	            
+	        }
+	        for(int i = 0; i < nums.length; i++) {
+	        	nums[i] = 0 - nums[i];
+	        }
+	    }
+	    
+	    int getNewI(int [] nums, int i) {
+	        if(i > (nums.length - 1)/2) {
+	            return (nums.length - i)*2 - 1;
+	        } else {
+	            return nums.length%2 > 0 ? (nums.length/2 - i)*2 : (nums.length/2 - i - 1)*2;
+	        }
+	    }
+	    
+	    void threeWaySort(int [] nums, int mid) {
+	        int zEnd = 0, tStart = nums.length - 1;
+	        for(int i = 0; i <= tStart; i++) {
+	            if(nums[i] < mid) {
+	                swap(nums, zEnd, i);
+	                zEnd++;
+	            } else if(nums[i] > mid) {
+	                swap(nums, i, tStart);
+	                tStart--;
+	                i--;
+	            }
+	        }
+	    }
+	    
+	    int findKth2(int [] nums, int k) {
+	        int start = 0, end = nums.length - 1;
+	        while(start <= end) {
+	            int m = partition2(nums, start, end);
+	            if(m == k - 1) {
+	                return nums[m];
+	            } else if (m > k - 1) {
+	                end = m - 1;
+	            } else {
+	                start = m + 1;
+	            }
+	        }
+	        return Integer.MAX_VALUE;
+	    }
+	    
+	    int partition2(int [] nums, int start, int end) {
+	        int t = nums[start];
+	        while(start < end) {
+	            while(end > start && nums[end] >= t) {
+	                end--;
+	            }
+	            if(end > start) {
+	                nums[start] = nums[end];
+	                start++;
+	            }
+	            while(end > start && nums[start] <= t) {
+	                start++;
+	            }
+	            if(end > start) {
+	                nums[end] = nums[start];
+	                end--;
+	            }
+	        }
+	        nums[start] = t;
+	        return start;
+	    }
+	    void swap(int [] a, int i, int j) {
+	        int t = a[i];
+	        a[i] = a[j];
+	        a[j] = t;
+	    }
 }
