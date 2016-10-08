580135a044147dba681d20f3325f661340b679cb
zsll <zsll@users.noreply.github.com>
2016-10-07 19:58:27 -0700

    [393. UTF-8 Validation][Time Limit Exceeded]committed by LeetCode Extension

--- 393-UTF-8-Validation/solution.java
+++ 393-UTF-8-Validation/solution.java
@@ -1,46 +1,40 @@
 public class Solution {
     public boolean validUtf8(int[] data) {
         boolean res = true;
-        int i = 0;
-        while (i < data.length) {
-            if(!startWith0(data[i])) {
-                int j = i + 1;
-                int startingOnes = getStarting1Num(data[i]);
-                if(startingOnes == 1) {
-                    /*single number 10010001*/
-                    res = false;
-                    break;
-                }
-                while(j < data.length && j - i < startingOnes && startWith10(data[j])) {
-                    j++;
-                }
-                if(j - i < startingOnes) {
+        if(data != null && data.length > 0) {
+            int i = 0;
+            while(i < data.length) {
+                int cur = data[i];
+                int starting1s = starting1s(cur);
+                if(starting1s == 0) {
+                    i++;
+                } else if(starting1s == 1) {
                     res = false;
                     break;
                 } else {
-                    i = j;
+                    int j = i + 1;
+                    for(; j <= Math.min(data.length, i + starting1s - 1); j++) {
+                        int starting1s2 = starting1s(data[j]);
+                        if(starting1s2 != 1) {
+                            break;
+                        }
+                    }
+                    if(j < i + starting1s) {
+                        res = false;
+                        break;
+                    }
                 }
-            } else {
-                i++;
             }
         }
         return res;
     }
     
-    boolean startWith0(int a) {
-        return (a & (1 << 7)) == 0;
-    }
-    
-    boolean startWith10(int a) {
-        return (a >> 6) == 2;
-    }
-    
-    int getStarting1Num(int a) {
-        int t = (1 << 7);
+    int starting1s(int i) {
+        int mask = 1 << 7;
         int res = 0;
-        while((t&a) > 0) {//NoteNote the & sign has lower priority than >
-            t = t >> 1;
+        while((mask & i) > 0) {
             res++;
+            mask = mask >>> 1;
         }
         return res;
     }
