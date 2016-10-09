aec2f0d6616e9a2b98c5920c348b51441d2e197f
zsll <zsll@users.noreply.github.com>
2016-10-08 23:16:24 -0700

    [354. Russian Doll Envelopes][Accepted]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -2,61 +2,41 @@ public class Solution {
     public int maxEnvelopes(int[][] envelopes) {
         int res = 0;
         if(envelopes != null && envelopes.length > 0) {
-            List<Envelope> envList = new ArrayList<Envelope>();
-            for(int i = 0; i < envelopes.length; i++) {
-                envList.add(new Envelope(envelopes[i][0], envelopes[i][1]));
-            }
-            Collections.sort(envList, new Comparator<Envelope>() {
-               @Override
-               public int compare(Envelope a, Envelope b) {
-                   /**
-                    * To avoid the following error since same width can't hold
-                    * Input:
-[[5,4],[6,4],[6,7],[2,3]]
-Output:
-4
-Expected:
-3*/
-                   if(a.width == b.width) {
-                       return Integer.compare(b.height, a.height);
-                   }
-                   return Integer.compare(a.width, b.width);
-               }
-            });
-            int len = envList.size();
-            int [] b = new int[len + 1];
-            res = 1;
-            b[1] = envList.get(0).height;
-            for(int i = 1; i < len; i++) {
-                if(b[res] < envList.get(i).height) {
-                    b[++res] = envList.get(i).height;
-                } else {
-                    int pos = searchPos(envList.get(i).height, b, res - 1);
-                    b[pos] = envList.get(i).height;
+            Arrays.sort(envelopes, new Comparator<int []>() {
+                public int compare(int [] a, int [] b) {
+                	if(a[0] != b[0]) {
+                		return a[0] - b[0];
+                	} else {
+                		return b[1] - a[1];
+                	}
                 }
+            });
+            int len = envelopes.length;
+            int [] b = new int[len + 1];	//b i is the smallest number that a lis of length i can be
+            //we can easily prove that this array is ascending
+            b[++res] = envelopes[0][1];
+            for(int i = 1; i < len; i++) {
+            	if(b[res] < envelopes[i][1]) {
+            		b[++res] = envelopes[i][1];
+            	} else {
+            		int toUpdate = findFirstGTIndex(b, res, envelopes[i][1]);
+            		b[toUpdate] = envelopes[i][1];
+            	}
             }
         }
         return res;
     }
     
-    int searchPos(int height, int [] b, int end) {
-        int start = 0, res = 0;
-        while(start <= end) {
-            int mid = start + (end - start >> 1);
-            if(b[mid] >= height) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return start;
-    }
-    
-    class Envelope {
-        int width, height;
-        public Envelope(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
+    int findFirstGTIndex(int [] b, int end, int target) {
+    	int start = 0;
+    	while(start <= end) {
+    		int mid = (end - start >> 1) + start;
+    		if(b[mid] >= target) {
+    			end = mid - 1;
+    		} else {
+    			start = mid + 1;
+    		}
+    	}
+    	return start;
     }
 }
