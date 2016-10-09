64c7e4e34fbf3c62fa348b26a585f939bfbd9583
zsll <zsll@users.noreply.github.com>
2016-10-09 00:43:09 -0700

    [402. Remove K Digits][Accepted]committed by LeetCode Extension

--- 402-Remove-K-Digits/solution.java
+++ 402-Remove-K-Digits/solution.java
@@ -1,40 +1,20 @@
 public class Solution {
     public String removeKdigits(String num, int k) {
-        String res = "";
-        /**
-         * Runtime Error Message:
-Line 11: java.lang.NumberFormatException: For input string: ""
-Last executed input:
-"10"
-2
-         */ 
-        if(num != null && k >= 0 && num.length() >= k) {
-            String next = num;  //NoteNote the initialization incase k = 0;
+        String res = "0";
+        if(num != null && num.length() > k && k >= 0) {
+            int i = 0;
+            StringBuffer sb = new StringBuffer(num);
             while(k > 0) {
-                next = remove1(num);
-                k--;
-                num = next;
-            }
-            /**
-             * Runtime Error Message:
-Line 18: java.lang.NumberFormatException: For input string: "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001001604269216566928220767474713135...
-Can't use integer conversion
-             */
-            /**
-             * Submission Result: Wrong Answer More Details 
-
-Input:
-"10"
-2
-Output:
-""
-Expected:
-"0"*/
-            
-            StringBuffer sb = new StringBuffer(next);
-            
-            if(sb.length() == 0) {
-                sb.append(0);
+                if(i == sb.length() - 1 || sb.charAt(i) > sb.charAt(i + 1)) {
+                    sb.deleteCharAt(i);
+                    k--;
+                    if(i == 0) {
+                        i--;
+                    } else {
+                        i -= 2;
+                    }
+                }
+                i++;
             }
             while(sb.length() > 1 && sb.charAt(0) == '0') {
                 sb.deleteCharAt(0);
@@ -42,18 +22,5 @@ Expected:
             res = sb.toString();
         }
         return res;
-    }
-    
-    String remove1(String s) {
-        StringBuffer sb = new StringBuffer(s);
-        int i = 0;
-        while(i < sb.length() - 1) {
-            if(sb.charAt(i) > sb.charAt(i + 1)) {
-                break;
-            }
-            i++;
-        }
-        sb.deleteCharAt(i);
-        return sb.toString();
     }
 }
