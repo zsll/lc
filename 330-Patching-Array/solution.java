1a988d2f1d08a2c1e7de49b6afe20573ed867fa3
zsll <zsll@users.noreply.github.com>
2016-10-09 13:21:01 -0700

    [330. Patching Array][Accepted]committed by LeetCode Extension

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,15 +1,19 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-	        long target = 1;    //NoteNote, if n is Integer.MAX_VALUE, out of scope
-	        int i = 0, res = 0;
-	        while (target <= n) {
-	            if(i < nums.length && target >= nums[i]) {
-	                target += nums[i++];
-	            } else {
-	                target *= 2;
-	                res++;
-	            }
-	        }
-	        return res;
-	    }
+        int res = 0;
+        if(nums != null) {
+            long toBePatched = 1;
+            int i = 0;
+            while(toBePatched <= n) {
+                if(i < nums.length && nums[i] <= toBePatched) {
+                    toBePatched += nums[i];
+                    i++;
+                } else {
+                    toBePatched = toBePatched*2;
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
 }
