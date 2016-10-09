22015908bde42359c660e8168c95a5fae07d4b65
zsll <zsll@users.noreply.github.com>
2016-10-09 12:29:47 -0700

    [363. Max Sum of Rectangle No Larger Than K][Time Limit Exceeded]committed by LeetCode Extension

--- 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.java
+++ 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.java
@@ -1,31 +1,37 @@
 public class Solution {
     public int maxSumSubmatrix(int[][] matrix, int k) {
-        int m = matrix.length, n = 0;
-         if (m > 0) n = matrix[0].length;
-         if (m * n == 0) return 0;
-         
-         int M = Math.max(m, n);
-         int N = Math.min(m, n);
-         
-         int ans = Integer.MIN_VALUE;
-         for (int x = 0; x < N; x++) {	//Start row/col
-             int sums[] = new int[M];//sum of col/row, updated every time
-             for (int y = x; y < N; y++) {	//End row/col
-                 TreeSet<Integer> set = new TreeSet<Integer>();
-                 int num = 0;
-                 for (int z = 0; z < M; z++) {
-                     sums[z] += m > n ? matrix[z][y] : matrix[y][z];
-                     num += sums[z];
-                     if (num <= k) 
-                    	 ans = Math.max(ans, num);
-                     Integer i = set.ceiling(num - k);//return the least number greater or equal to num - k
-                     //find the max number that is smaller than or equal to num minus k
-                     if (i != null) //Note ceiling might return null
-                    	 ans = Math.max(ans, num - i);
-                     set.add(num);
-                 }
-             }
-         }
-         return ans;
+        int res = -1;
+        if(matrix != null && matrix.length > 0 && matrix[0].length > 0) {
+            int h = matrix.length, w = matrix[0].length;
+            res = Integer.MIN_VALUE;
+            for(int i = 0; i < h; i++) {
+                int [] colSum = new int[w];
+                for(int j = i; j < h; j++) {
+                    //Find sum a - sum b <= k where b is smaller than a
+                    //sum b >= sum a - k
+                    TreeSet<Integer> s = new TreeSet<Integer>();
+                    int sum = 0;
+                    for(int p = 0; p < w; p++) {
+                        colSum[p] += matrix[j][p];
+                        sum += colSum[p];
+                        if(sum <= k) {
+                            res = Math.max(sum, res);
+                        }
+                        Integer pre = s.higher(sum - k - 1);
+                        //Integer i = set.ceiling(num - k);//return the least number greater or equal to num - k
+                        if(pre != null) {
+                            //if(sum - pre <= k) {  //Not needed
+                                res = Math.max(sum - pre, res);
+                            //}
+                        }
+                        s.add(sum);
+                    }
+                }
+            }
+            if(res == Integer.MIN_VALUE) {
+                res = -1;
+            }
+        }
+        return res;
     }
 }
