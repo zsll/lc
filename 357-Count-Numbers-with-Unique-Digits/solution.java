e4858a569e4cc28b0c42fff41035bd7008d6d2e0
zsll <zsll@users.noreply.github.com>
2016-10-09 09:58:02 -0700

    [357. Count Numbers with Unique Digits][Accepted]committed by LeetCode Extension

--- 357-Count-Numbers-with-Unique-Digits/solution.java
+++ 357-Count-Numbers-with-Unique-Digits/solution.java
@@ -1,21 +1,24 @@
 public class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        int res = 1;
+        //f1 10, f2 9*(10 - 1) + 1
+        //3 9*9*(10 -2) + f2 ... fi 9*9*...(11 - i) + f i - 1
+        int res = 0;
+        if(n == 0) {
+            res = 1;    //NoteNote
+        }
         
-        if(n > 0) {
+        if(n >= 1 && n < 11) {
             int [] f = new int[n + 1];
-            f[1] = 10;  //only 1 digit
-            res = f[1];
+            f[1] = 10;
             if(n > 1) {
-                f[2] = 9 * 9;   //from 10 to 99
-                res += f[2];
-                //f[3] from 100 to 999, 9*9*8
-                //if n = 10, not possible
-                for(int i = 3; i <= Math.min(n, 10); i++) {
-                    f[i] = f[i - 1]*(11 - i);
-                    res += f[i];
+                f[2] = 91;
+                int base = 81;
+                for(int i = 3; i <= n; i++) {
+                    base *= (11 - i);
+                    f[i] = f[i - 1] + base;
                 }
             }
+            res = f[n];
         }
         return res;
     }
