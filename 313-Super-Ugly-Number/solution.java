921afbe46a12c56eb37e82126ed959ffdfd1774d
zsll <zsll@users.noreply.github.com>
2016-10-09 09:47:51 -0700

    [313. Super Ugly Number][Accepted]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int res = 0;
+        List<Integer> res = new ArrayList<Integer>();
         if(n > 0 && primes != null && primes.length > 0) {
             PriorityQueue<PrimeProduct> q = new PriorityQueue<PrimeProduct>(10, new Comparator<PrimeProduct>() {
                public int compare(PrimeProduct a, PrimeProduct b) {
@@ -10,24 +10,30 @@ public class Solution {
             for(int p : primes) {
                 q.offer(new PrimeProduct(p));
             }
+            res.add(1);
+            n--;
             while(n > 0) {
-                res = q.peek().product;
-                while(q.peek().product == res) {
+                res.add(q.peek().product);  //smallest get(pos)*prime
+                while(q.peek().product == res.get(res.size() - 1)) {
                     PrimeProduct cur = q.poll();
-                    cur.product = res*cur.prime;
+                    cur.pos++;
+                    cur.product = res.get(cur.pos)*cur.prime;
                     q.offer(cur);
                 }
                 n--;
             }
         }
-        return res;
+        return res.get(res.size() - 1);
     }
     
     class PrimeProduct {
         int prime;
         int product;
+        int pos;    //pos in res list
         public PrimeProduct(int p) {
             this.prime = p;
+            this.product = p;//NoteNote: initialize (This is product of list.get(pos)*prime
+            pos = 0;
         }
     }
 }
