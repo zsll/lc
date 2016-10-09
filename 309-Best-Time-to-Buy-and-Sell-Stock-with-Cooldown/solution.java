4afa5a5689a715f514a289a98482553380b695a0
zsll <zsll@users.noreply.github.com>
2016-10-08 18:44:07 -0700

    [309. Best Time to Buy and Sell Stock with Cooldown][Accepted]committed by LeetCode Extension

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.java
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.java
@@ -1,22 +1,34 @@
 public class Solution {
+    // convert the price diff to blocks, diff i = price[i + 1] - price[i]
+    // sell, rest, buy, sell i = diff i - 1 + 
+    //s[i] is the profit of selling on i th dy (must take the i th block), g[i] is the global max on day i
+    //s[i] = Math.max(s[i - 1] + diff[i - 1], g[i - 3] + diff[i - 1])
+    //g[i] = Math.max(g[i - 1], s[i]);
+    //initialization:
+    //s 0 = 0, g 0 = 0,
+    //s 1 = diff 0, g 1 = Math.max(0, s1)
+    //s 2 = Math.max(diff 1, diff 0 + diff 1), g 2 = Math.max(g 1, s 2)
     public int maxProfit(int[] prices) {
         int res = 0;
-        if(prices != null && prices.length > 1) {
-            int l = prices.length - 1;  //There are prices.length - 1 nubmers (diff between prices i minus prices minus 1
-            //we need to find the max sum and make sure the minimum distance between adjacent diffs are larger than 1)
-            int [] local = new int[l + 1];  //local i means max sum end with ith diff
-            int [] global = new int[l + 1]; //global i means max sum end with any one, not has to be ith diff
-            local[1] = prices[1] - prices[0];
-            global[1] = Math.max(local[1], 0);
-            if(prices.length > 2) {
-                local[2] = Math.max(prices[2] - prices[1], prices[2] - prices[0]);
-                global[2] = Math.max(local[2], global[1]);
-                for(int i = 3; i <= l; i++) {
-                    local[i] = Math.max(local[i - 1] + prices[i] - prices[i - 1], global[i-3] + prices[i] - prices[i - 1]);
-                    global[i] = Math.max(local[i], global[i - 1]);
+        if(prices != null && prices.length > 1) {   //at least there is 1 block
+            int len = prices.length;
+            int [] diff = new int[len - 1];
+            for(int i = 0; i < len - 1; i++) {
+                diff[i] = prices[i + 1] - prices[i];
+            }
+            int [] s = new int[len - 1 + 1];    //profit of taking i th
+            int [] g = new int[len - 1 + 1];    //global for first i diff
+            s[1] = diff[0];
+            g[1] = Math.max(0, s[1]);
+            if(len > 2) {
+                s[2] = Math.max(diff[0], 0) + diff[1];
+                g[2] = Math.max(g[1], s[2]);
+                for(int i = 3; i < len; i++) {
+                    s[i] = Math.max(s[i - 1] + diff[i - 1], g[i - 3] + diff[i - 1]);
+                    g[i] = Math.max(g[i - 1], s[i]);
                 }
             }
-            res = global[l];
+            res = g[len - 1];
         }
         return res;
     }
