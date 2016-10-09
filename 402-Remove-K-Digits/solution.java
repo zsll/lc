fc6fed2455f89180c3aed5c9adcc235d0d60a6d4
zsll <zsll@users.noreply.github.com>
2016-10-09 00:31:01 -0700

    [402. Remove K Digits][Accepted]committed by LeetCode Extension

--- 402-Remove-K-Digits/solution.java
+++ 402-Remove-K-Digits/solution.java
@@ -8,14 +8,38 @@ Last executed input:
 "10"
 2
          */ 
-        if(num != null && k >= 0 && num.length() > k) {
+        if(num != null && k >= 0 && num.length() >= k) {
             String next = num;  //NoteNote the initialization incase k = 0;
             while(k > 0) {
                 next = remove1(num);
                 k--;
                 num = next;
             }
-            res = Integer.toString(Integer.parseInt(next));
+            /**
+             * Runtime Error Message:
+Line 18: java.lang.NumberFormatException: For input string: "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001001604269216566928220767474713135...
+Can't use integer conversion
+             */
+            /**
+             * Submission Result: Wrong Answer More Details 
+
+Input:
+"10"
+2
+Output:
+""
+Expected:
+"0"*/
+            
+            StringBuffer sb = new StringBuffer(next);
+            
+            if(sb.length() == 0) {
+                sb.append(0);
+            }
+            while(sb.length() > 1 && sb.charAt(0) == '0') {
+                sb.deleteCharAt(0);
+            }
+            res = sb.toString();
         }
         return res;
     }
