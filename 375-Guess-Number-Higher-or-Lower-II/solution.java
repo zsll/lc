84f0e1c5c71f0a11b10916451e421c757a8e1900
zsll <zsll@users.noreply.github.com>
2016-10-09 10:46:44 -0700

    [375. Guess Number Higher or Lower II][Accepted]committed by LeetCode Extension

--- 375-Guess-Number-Higher-or-Lower-II/solution.java
+++ 375-Guess-Number-Higher-or-Lower-II/solution.java
@@ -1,27 +1,31 @@
 public class Solution {
-    /**
-     *  Use dp[i][j] denotes the min amount of money we need to pay by guessing from i to j, j >= i
-     *  dp[i][i] equals 0
-     *  dp[i][i + 1] equals i
-     *  dp[i][j] equals Math.min(dp[i][j], k + Math.max(dp[i][k - 1], dp[k + 1][j])), k > i && k < j
-     *  result is dp 1 n
-     */
+    //f i j is the cost to guess from i to j
+    //when j - i = 0, is 0
+    //when j - i = 1, is i
+    //else k scan from i to j, if I guess k, 
+    //if correct, it's 0, If not max would be Max of f[i, k - 1] ,  f[k + 1, j] + k
     public int getMoneyAmount(int n) {
         int res = 0;
         if(n > 1) {
             int [][] dp = new int[n + 1][n + 1];
             for(int i = 1; i <= n; i++) {
                 dp[i][i] = 0;
-                if(i < n) { //NoteNote
+                if(i < n) {
                     dp[i][i + 1] = i;
                 }
             }
-            for(int len = 3; len <= n; len++) {
-                for(int start = 1; start <= n - len + 1; start++) {
-                    int end = start + len - 1;
-                    dp[start][end] = Integer.MAX_VALUE;
-                    for(int k = start + 1; k < end; k++) {
-                        dp[start][end] = Math.min(dp[start][end], k + Math.max(dp[start][k - 1], dp[k + 1][end]));
+            for(int diff = 2; diff < n; diff++) {
+                for(int row = 1; row + diff <= n; row++) {
+                    int col = diff + row;
+                    dp[row][col] = Integer.MAX_VALUE;
+                    for(int k = row; k <= col; k++) {
+                        if(k == row) {
+                            dp[row][col] = Math.min(dp[row][col], k + dp[k + 1][col]);
+                        } else if (k == col) {
+                            dp[row][col] = Math.min(dp[row][col], k + dp[row][k - 1]);
+                        } else {
+                            dp[row][col] = Math.min(dp[row][col], Math.max(k + dp[row][k - 1], k + dp[k + 1][col]));
+                        }
                     }
                 }
             }
