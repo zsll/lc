bcda29d7d0dbb328c693abc3197fd17ba05d0216
zsll <zsll@users.noreply.github.com>
2016-10-09 19:29:20 -0700

    [418. Sentence Screen Fitting][Accepted]committed by LeetCode Extension

---
+++ 418-Sentence-Screen-Fitting/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    //Assume there are k words
+    //dp[i] means when starting from word i, how mnay lines it would need and
+    //how many spaces it would leave in last line
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        String s = String.join(" ", sentence) + " ";
+        int start = 0, l = s.length();
+        for (int i = 0; i < rows; i++) {
+            start += cols;
+            if (s.charAt(start % l) == ' ') {
+                start++;
+            } else {
+                while (start > 0 && s.charAt((start-1) % l) != ' ') {
+                    start--;
+                }
+            }
+        }
+        
+        return start / s.length();
+    }
+}
