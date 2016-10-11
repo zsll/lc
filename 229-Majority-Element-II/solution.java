8c25b56083f44d29ffa0289bc887b260ab287453
zsll <zsll@users.noreply.github.com>
2016-10-11 00:25:38 -0700

    [229. Majority Element II][Accepted]committed by LeetCode Extension

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -3,13 +3,13 @@ public class Solution {
         
     List<Integer> result = new ArrayList<Integer>();
  
-    Integer n1=null, n2=null;
+    int n1=0, n2=0;
     int c1=0, c2=0;
  
     for(int i: nums){
-        if(c1 > 0 && i==n1.intValue()){
+        if(c1 > 0 && i==n1){
             c1++;
-        }else if(c2 > 0 && i==n2.intValue()){
+        }else if(c2 > 0 && i==n2){
             c2++;
         }else if(c1==0){
             c1=1;
@@ -26,9 +26,9 @@ public class Solution {
     c1=c2=0;
  
     for(int i: nums){
-        if(i==n1.intValue()){
+        if(i==n1){
             c1++;
-        }else if(i==n2.intValue()){
+        }else if(i==n2){
             c2++;
         }
     }
